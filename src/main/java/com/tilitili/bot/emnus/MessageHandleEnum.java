55fe78f376c708b150e19ac760685332535fd23b
黄克凡 <w545459363@126.com>
2021-12-29 18:14:49 +0800

    tag

--- src/main/java/com/tilitili/bot/emnus/MessageHandleEnum.java
+++ src/main/java/com/tilitili/bot/emnus/MessageHandleEnum.java
@@ -4,25 +4,28 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static com.tilitili.common.emnus.SendTypeEmum.*;
+
 public enum MessageHandleEnum {
 
-	AddRecommendHandle(		"AddRecommendHandle",		Arrays.asList("推荐","tj"),				Arrays.asList("FriendMessage"),		0,		""),
-	AddSubscriptionHandle(	"AddSubscriptionHandle",	Arrays.asList("关注","gz"),				Arrays.asList("FriendMessage"),		0,		"关注b站up，关注后可以获得开播提醒和动态推送。格式：(gz[换行]uid=1)"),
-	BeautifyJsonHandle(		"BeautifyJsonHandle",		Arrays.asList("Json","json"),			Arrays.asList("FriendMessage"),		0,		""),
-	CalendarHandle(			"CalendarHandle",			Arrays.asList("日程表","rc"),			Arrays.asList("FriendMessage"),		0,		"日程表，在指定时间提醒做某事。格式：（xxx叫我xxx）"),
-	FindImageHandle(		"FindImageHandle",		Arrays.asList("找图","zt"),				Arrays.asList("FriendMessage"),		0,		"查找原图。格式(zt[换行][图片])"),
-	FranslateHandle(		"FranslateHandle",		Arrays.asList("翻译","fy"),				Arrays.asList("FriendMessage"),		0,		"翻译文本或图片。格式(fy[换行]hello!)"),
-	HelpHandle(				"HelpHandle",				Arrays.asList("帮助","help","?","？"),	Arrays.asList("FriendMessage"),		0,		"获取帮助"),
-	PatternStringHandle(	"PatternStringHandle",	Arrays.asList("正则","zz"),				Arrays.asList("FriendMessage"),		0,		""),
-	RenameHandle(			"RenameHandle",			Collections.emptyList(),				Arrays.asList("GroupMessage"),		2,		""),
-	NoBakaHandle(			"NoBakaHandle",			Collections.emptyList(),				Arrays.asList("GroupMessage"),		1,		""),
-	RepeatHandle(			"RepeatHandle",			Collections.emptyList(),				Arrays.asList("GroupMessage"),		-1,	""),
-	VoiceHandle(			"VoiceHandle",			Arrays.asList("说","s"),				Arrays.asList("GroupMessage"),		0,		"文本转语音(日语)(群聊)。格式:(s[换行]你好！)"),
-	PixivHandle(			"PixivHandle",			Arrays.asList("色图","st","ss","bs"),	Arrays.asList("GroupMessage"),		1,		"色图！(群聊)。格式:(st loli)"),
-	RecallHandle(			"RecallHandle",			Arrays.asList("撤回","ch"),				Arrays.asList("FriendMessage"),		0,		""),
-	ConfigHandle(			"ConfigHandle",			Arrays.asList("配置","pz"),				Arrays.asList("FriendMessage"),		0,		""),
-	CfcxHandle(				"CfcxHandle",				Arrays.asList("成分查询","cfcx"),		Arrays.asList("GroupMessage"),		0,		"成分查询。格式:(cfcx Jadlokin_Scarlet)"),
-	NewVideoHandle(			"NewVideoHandle",			Arrays.asList("新视频","nv"),			Arrays.asList("GuildMessage"),		0,		"随机获取昨日新增视频。格式:(.nv)"),
+	AddRecommendHandle(		"AddRecommendHandle",		Arrays.asList("推荐","tj"),				Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		""),
+	AddSubscriptionHandle(	"AddSubscriptionHandle",	Arrays.asList("关注","gz"),				Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		"关注b站up，关注后可以获得开播提醒和动态推送。格式：(gz[换行]uid=1)"),
+	BeautifyJsonHandle(		"BeautifyJsonHandle",		Arrays.asList("Json","json"),			Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		""),
+	CalendarHandle(			"CalendarHandle",			Arrays.asList("日程表","rc"),			Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		"日程表，在指定时间提醒做某事。格式：（xxx叫我xxx）"),
+	FindImageHandle(		"FindImageHandle",		Arrays.asList("找图","zt"),				Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		"查找原图。格式(zt[换行][图片])"),
+	FranslateHandle(		"FranslateHandle",		Arrays.asList("翻译","fy"),				Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		"翻译文本或图片。格式(fy[换行]hello!)"),
+	HelpHandle(				"HelpHandle",				Arrays.asList("帮助","help","?","？"),	Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		"获取帮助"),
+	PatternStringHandle(	"PatternStringHandle",	Arrays.asList("正则","zz"),				Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		""),
+	RenameHandle(			"RenameHandle",			Collections.emptyList(),				Arrays.asList(GROUP_MESSAGE),					2,		""),
+	NoBakaHandle(			"NoBakaHandle",			Collections.emptyList(),				Arrays.asList(GROUP_MESSAGE),					1,		""),
+	RepeatHandle(			"RepeatHandle",			Collections.emptyList(),				Arrays.asList(GROUP_MESSAGE),					-1,	""),
+	VoiceHandle(			"VoiceHandle",			Arrays.asList("说","s"),				Arrays.asList(GROUP_MESSAGE),					0,		"文本转语音(日语)(群聊)。格式:(s[换行]你好！)"),
+	PixivHandle(			"PixivHandle",			Arrays.asList("色图","st","ss","bs"),	Arrays.asList(GROUP_MESSAGE),					1,		"色图！(群聊)。格式:(st loli)"),
+	RecallHandle(			"RecallHandle",			Arrays.asList("撤回","ch"),				Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		""),
+	ConfigHandle(			"ConfigHandle",			Arrays.asList("配置","pz"),				Arrays.asList(FRIEND_MESSAGE, TEMP_MESSAGE),	0,		""),
+	CfcxHandle(				"CfcxHandle",				Arrays.asList("成分查询","cfcx"),		Arrays.asList(GROUP_MESSAGE),					0,		"成分查询。格式:(cfcx Jadlokin_Scarlet)"),
+	NewVideoHandle(			"NewVideoHandle",			Arrays.asList("新视频","nv"),			Arrays.asList(GUILD_MESSAGE),					0,		"随机获取昨日新增视频。格式:(nv)"),
+	TagHandle(				"TagHandle",				Arrays.asList("tag"),					Arrays.asList(GROUP_MESSAGE),					0,		"查询指定pid的tag。格式:(tag 1231)"),
 	;
 
 	private final String name;
---
+++ src/main/java/com/tilitili/bot/service/mirai/TagHandle.java
@@ -0,0 +1,37 @@
+package com.tilitili.bot.service.mirai;
+
+import com.tilitili.bot.emnus.MessageHandleEnum;
+import com.tilitili.bot.entity.bot.BotMessageAction;
+import com.tilitili.common.entity.PixivTag;
+import com.tilitili.common.entity.query.PixivTagQuery;
+import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.mapper.tilitili.PixivTagMapper;
+import com.tilitili.common.utils.Asserts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class TagHandle extends ExceptionRespMessageHandle {
+	private final PixivTagMapper pixivTagMapper;
+
+	@Autowired
+	public TagHandle(PixivTagMapper pixivTagMapper) {
+		this.pixivTagMapper = pixivTagMapper;
+	}
+
+	@Override
+	public MessageHandleEnum getType() {
+		return MessageHandleEnum.TagHandle;
+	}
+
+	@Override
+	public BotMessage handleMessage(BotMessageAction messageAction) {
+		String pid = messageAction.getParamOrDefault("pid", messageAction.getValue());
+		Asserts.notBlank(pid, "格式错啦(pid)");
+		List<PixivTag> tagList = pixivTagMapper.getPixivTagByCondition(new PixivTagQuery().setPid(pid));
+		return BotMessage.simpleTextMessage(String.format("[%s]的tag有：%s", pid, tagList.stream().map(PixivTag::getTag).collect(Collectors.joining(" "))));
+	}
+}
