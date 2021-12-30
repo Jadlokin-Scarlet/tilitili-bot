f993cf95274d3835827c4a216e40108765db0bd5
黄克凡 <w545459363@126.com>
2021-12-30 11:22:43 +0800

    subscription

--- src/main/java/com/tilitili/bot/service/mirai/AddSubscriptionHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/AddSubscriptionHandle.java
@@ -2,8 +2,10 @@ package com.tilitili.bot.service.mirai;
 
 import com.tilitili.bot.emnus.MessageHandleEnum;
 import com.tilitili.bot.entity.bot.BotMessageAction;
+import com.tilitili.common.emnus.SendTypeEmum;
 import com.tilitili.common.emnus.TaskReason;
 import com.tilitili.common.entity.Subscription;
+import com.tilitili.common.entity.query.SubscriptionQuery;
 import com.tilitili.common.entity.view.bot.BotMessage;
 import com.tilitili.common.entity.view.message.SimpleTask;
 import com.tilitili.common.manager.TaskManager;
@@ -35,16 +37,23 @@ public class AddSubscriptionHandle extends ExceptionRespMessageHandle {
     @Override
     public BotMessage handleMessage(BotMessageAction messageAction) {
         String uid = messageAction.getParamOrDefault("uid", messageAction.getValue());
-        Long qq = messageAction.getBotMessage().getQq();
-        Long group = messageAction.getBotMessage().getGroup();
+        BotMessage botMessage = messageAction.getBotMessage();
+
+        Long qq = botMessage.getQq();
+        Long group = botMessage.getGroup();
+        String guildId = botMessage.getChannel() == null? botMessage.getGuildId(): botMessage.getChannel().guildId;
+        String channelId = botMessage.getChannelId() == null? botMessage.getChannelId(): botMessage.getChannel().channelId;
+        String sendType = botMessage.getSendType();
 
         Asserts.notBlank(uid, "格式错啦(uid)");
 
-        int oldCount = subscriptionMapper.countSubscriptionByCondition(new Subscription().setType(1).setValue(uid).setSendQq(qq));
+        Long qqWithoutGroup = SendTypeEmum.GROUP_MESSAGE.equals(sendType)? null: qq;
+        SubscriptionQuery subscriptionQuery = new SubscriptionQuery().setType(1).setValue(uid).setSendType(sendType).setSendGroup(group).setSendQq(qqWithoutGroup).setSendGuild(guildId).setSendChannel(channelId);
+        int oldCount = subscriptionMapper.countSubscriptionByCondition(subscriptionQuery);
         Asserts.isTrue(oldCount == 0, "已经关注了哦。");
 
-        Subscription add = new Subscription().setValue(uid).setType(1).setSendGroup(group).setSendQq(qq).setSendType(group == null? "FriendMessage": "TempMessage");
-        subscriptionMapper.insertSubscription(add);
+        Subscription add = new Subscription().setValue(uid).setType(1).setSendType(sendType).setSendGroup(group).setSendQq(qqWithoutGroup).setSendGuild(guildId).setSendChannel(channelId);
+        subscriptionMapper.addSubscriptionSelective(add);
 
         SimpleTask simpleTask = new SimpleTask().setValueList(Collections.singletonList(uid)).setReason(TaskReason.SUPPLEMENT_VIDEO_OWNER.value);
         taskManager.simpleSpiderVideo(simpleTask);
