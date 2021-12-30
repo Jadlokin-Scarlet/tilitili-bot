0c3a862bf6eecf219c6c3924bef761970eeb45cc
黄克凡 <w545459363@126.com>
2021-12-30 15:46:42 +0800

    gz

--- src/main/java/com/tilitili/bot/service/mirai/subscription/AddSubscriptionHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/subscription/AddSubscriptionHandle.java
@@ -4,30 +4,32 @@ import com.tilitili.bot.emnus.MessageHandleEnum;
 import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.bot.service.mirai.ExceptionRespMessageHandle;
 import com.tilitili.common.emnus.SendTypeEmum;
-import com.tilitili.common.emnus.TaskReason;
+import com.tilitili.common.entity.Owner;
 import com.tilitili.common.entity.Subscription;
 import com.tilitili.common.entity.query.SubscriptionQuery;
 import com.tilitili.common.entity.view.bot.BotMessage;
-import com.tilitili.common.entity.view.message.SimpleTask;
-import com.tilitili.common.manager.TaskManager;
+import com.tilitili.common.manager.BilibiliManager;
 import com.tilitili.common.mapper.tilitili.SubscriptionMapper;
 import com.tilitili.common.utils.Asserts;
+import com.tilitili.common.utils.StreamUtil;
+import com.tilitili.common.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class AddSubscriptionHandle extends ExceptionRespMessageHandle {
     private final SubscriptionMapper subscriptionMapper;
-    private final TaskManager taskManager;
+    private final BilibiliManager bilibiliManager;
 
     @Autowired
-    public AddSubscriptionHandle(SubscriptionMapper subscriptionMapper, TaskManager taskManager) {
+    public AddSubscriptionHandle(SubscriptionMapper subscriptionMapper, BilibiliManager bilibiliManager) {
         this.subscriptionMapper = subscriptionMapper;
-        this.taskManager = taskManager;
+        this.bilibiliManager = bilibiliManager;
     }
 
     @Override
@@ -37,7 +39,7 @@ public class AddSubscriptionHandle extends ExceptionRespMessageHandle {
 
     @Override
     public BotMessage handleMessage(BotMessageAction messageAction) {
-        String uid = messageAction.getParamOrDefault("uid", messageAction.getValue());
+        String key = messageAction.getParamOrDefault("key", messageAction.getValue());
         BotMessage botMessage = messageAction.getBotMessage();
 
         Long qq = botMessage.getQq();
@@ -46,18 +48,30 @@ public class AddSubscriptionHandle extends ExceptionRespMessageHandle {
         String channelId = botMessage.getChannelId() == null? botMessage.getChannelId(): botMessage.getChannel().channelId;
         String sendType = botMessage.getSendType();
 
-        Asserts.notBlank(uid, "格式错啦(uid)");
+        Asserts.notBlank(key, "格式错啦(key)");
+
+        Owner owner;
+        if (StringUtils.isNumber(key)) {
+            owner = bilibiliManager.getUserWithCache(Long.valueOf(key));
+        } else {
+            List<Owner> ownerList = bilibiliManager.searchUserWithCache(key);
+            List<Owner> filterOwnerList = ownerList.stream().filter(StreamUtil.isEqual(Owner::getName, key)).collect(Collectors.toList());
+            Asserts.notEmpty(filterOwnerList, "找不到用户");
+            Asserts.checkEquals(filterOwnerList.size(), 1, "有重名(%s)", filterOwnerList.stream().map(Owner::getName).collect(Collectors.joining(",")));
+            owner = filterOwnerList.get(0);
+        }
+        Long uid = owner.getUid();
+        String name = owner.getName();
+        Long roomId = owner.getRoomId();
 
         Long qqWithoutGroup = SendTypeEmum.GROUP_MESSAGE.equals(sendType)? null: qq;
-        SubscriptionQuery subscriptionQuery = new SubscriptionQuery().setStatus(0).setType(1).setValue(uid).setSendType(sendType).setSendGroup(group).setSendQq(qqWithoutGroup).setSendGuild(guildId).setSendChannel(channelId);
+        SubscriptionQuery subscriptionQuery = new SubscriptionQuery().setStatus(0).setType(1).setValue(String.valueOf(uid)).setSendType(sendType).setSendGroup(group).setSendQq(qqWithoutGroup).setSendGuild(guildId).setSendChannel(channelId);
         int oldCount = subscriptionMapper.countSubscriptionByCondition(subscriptionQuery);
         Asserts.isTrue(oldCount == 0, "已经关注了哦。");
 
-        Subscription add = new Subscription().setValue(uid).setType(1).setSendType(sendType).setSendGroup(group).setSendQq(qqWithoutGroup).setSendGuild(guildId).setSendChannel(channelId);
+        Subscription add = new Subscription().setValue(String.valueOf(uid)).setType(1).setSendType(sendType).setSendGroup(group).setSendQq(qqWithoutGroup).setSendGuild(guildId).setSendChannel(channelId).setName(name).setRoomId(roomId);
         subscriptionMapper.addSubscriptionSelective(add);
 
-        SimpleTask simpleTask = new SimpleTask().setValueList(Collections.singletonList(uid)).setReason(TaskReason.SUPPLEMENT_VIDEO_OWNER.value);
-        taskManager.simpleSpiderVideo(simpleTask);
-        return BotMessage.simpleTextMessage("关注成功！");
+        return BotMessage.simpleTextMessage(String.format("关注%s成功！", name));
     }
 }
--- src/main/java/com/tilitili/bot/service/mirai/subscription/DeleteSubscriptionHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/subscription/DeleteSubscriptionHandle.java
@@ -4,23 +4,30 @@ import com.tilitili.bot.emnus.MessageHandleEnum;
 import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.bot.service.mirai.ExceptionRespMessageHandle;
 import com.tilitili.common.emnus.SendTypeEmum;
+import com.tilitili.common.entity.Owner;
 import com.tilitili.common.entity.Subscription;
 import com.tilitili.common.entity.query.SubscriptionQuery;
 import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.manager.BilibiliManager;
 import com.tilitili.common.mapper.tilitili.SubscriptionMapper;
 import com.tilitili.common.utils.Asserts;
+import com.tilitili.common.utils.StreamUtil;
+import com.tilitili.common.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class DeleteSubscriptionHandle extends ExceptionRespMessageHandle {
 	private final SubscriptionMapper subscriptionMapper;
+	private final BilibiliManager bilibiliManager;
 
 	@Autowired
-	public DeleteSubscriptionHandle(SubscriptionMapper subscriptionMapper) {
+	public DeleteSubscriptionHandle(SubscriptionMapper subscriptionMapper, BilibiliManager bilibiliManager) {
 		this.subscriptionMapper = subscriptionMapper;
+		this.bilibiliManager = bilibiliManager;
 	}
 
 	@Override
@@ -30,7 +37,7 @@ public class DeleteSubscriptionHandle extends ExceptionRespMessageHandle {
 
 	@Override
 	public BotMessage handleMessage(BotMessageAction messageAction) {
-		String uid = messageAction.getParamOrDefault("uid", messageAction.getValue());
+		String key = messageAction.getParamOrDefault("key", messageAction.getValue());
 		BotMessage botMessage = messageAction.getBotMessage();
 
 		Long qq = botMessage.getQq();
@@ -39,10 +46,23 @@ public class DeleteSubscriptionHandle extends ExceptionRespMessageHandle {
 		String channelId = botMessage.getChannelId() == null? botMessage.getChannelId(): botMessage.getChannel().channelId;
 		String sendType = botMessage.getSendType();
 
-		Asserts.notBlank(uid, "格式错啦(uid)");
+		Asserts.notBlank(key, "格式错啦(key)");
+
+		Owner owner;
+		if (StringUtils.isNumber(key)) {
+			owner = bilibiliManager.getUserWithCache(Long.valueOf(key));
+		} else {
+			List<Owner> ownerList = bilibiliManager.searchUserWithCache(key);
+			List<Owner> filterOwnerList = ownerList.stream().filter(StreamUtil.isEqual(Owner::getName, key)).collect(Collectors.toList());
+			Asserts.notEmpty(filterOwnerList, "找不到用户");
+			Asserts.checkEquals(filterOwnerList.size(), 1, "有重名(%s)", filterOwnerList.stream().map(Owner::getName).collect(Collectors.joining(",")));
+			owner = filterOwnerList.get(0);
+		}
+		Long uid = owner.getUid();
+		String name = owner.getName();
 
 		Long qqWithoutGroup = SendTypeEmum.GROUP_MESSAGE.equals(sendType)? null: qq;
-		SubscriptionQuery subscriptionQuery = new SubscriptionQuery().setStatus(0).setType(1).setValue(uid).setSendType(sendType).setSendGroup(group).setSendQq(qqWithoutGroup).setSendGuild(guildId).setSendChannel(channelId);
+		SubscriptionQuery subscriptionQuery = new SubscriptionQuery().setStatus(0).setType(1).setValue(String.valueOf(uid)).setSendType(sendType).setSendGroup(group).setSendQq(qqWithoutGroup).setSendGuild(guildId).setSendChannel(channelId);
 		List<Subscription> oldList = subscriptionMapper.getSubscriptionByCondition(subscriptionQuery);
 		Asserts.notEmpty(oldList, "还没关注哦。");
 		Asserts.checkEquals(oldList.size(), 1, "不太对劲。");
@@ -50,6 +70,6 @@ public class DeleteSubscriptionHandle extends ExceptionRespMessageHandle {
 
 		subscriptionMapper.updateSubscriptionSelective(new Subscription().setId(old.getId()).setStatus(-1));
 
-		return BotMessage.simpleTextMessage("搞定。");
+		return BotMessage.simpleTextMessage(String.format("取关%s成功", name));
 	}
 }
