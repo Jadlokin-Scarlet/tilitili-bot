2a3ab53ec69c6c1a09f8b4f56234990018012bb6
黄克凡 <w545459363@126.com>
2022-01-04 11:09:41 +0800

    help

--- src/main/java/com/tilitili/bot/service/mirai/HelpHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/HelpHandle.java
@@ -4,12 +4,13 @@ import com.tilitili.bot.emnus.MessageHandleEnum;
 import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.common.emnus.SendTypeEmum;
 import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class HelpHandle extends ExceptionRespMessageHandle {
@@ -30,15 +31,18 @@ public class HelpHandle extends ExceptionRespMessageHandle {
     public BotMessage handleMessage(BotMessageAction messageAction) {
         String sendType = messageAction.getBotMessage().getSendType();
         String guildprefix = sendType.equals(SendTypeEmum.Guild_Message.sendType)? ".": "";
-        List<MessageHandleEnum> hideHandleList = Arrays.asList(MessageHandleEnum.AddRecommendHandle, MessageHandleEnum.RecallHandle, MessageHandleEnum.PatternStringHandle, MessageHandleEnum.BeautifyJsonHandle, MessageHandleEnum.ConfigHandle);
 
-        List<MessageHandleEnum> filterHandleList = handleList.stream().map(BaseMessageHandle::getType).filter(e -> e.getSendType().contains(sendType)).filter(e -> !hideHandleList.contains(e)).collect(Collectors.toList());
+        // 筛选：不支持的发送方式不显示，未注册bean不显示，简介为空则不显示
+        Stream<MessageHandleEnum> filterHandleList = handleList.stream().map(BaseMessageHandle::getType).filter(e -> e.getSendType().contains(sendType)).filter(e -> StringUtils.isNotBlank(e.getDescription()));
 
-        StringBuilder stringBuilder = new StringBuilder("咱可以帮你做这些事！\n");
+        Stream<String> lineList = filterHandleList.map(handle -> {
+            String key = handle.getKeyword().stream().map(a -> guildprefix + a).collect(Collectors.joining(","));
+            return String.format("%s：%s", key, handle.getDescription());
+        });
 
-        String body = filterHandleList.stream().filter(a->!a.getDescription().isEmpty()).map(handle ->
-                handle.getKeyword().stream().map(a -> guildprefix + a).collect(Collectors.joining(",")) + "：" + handle.getDescription()
-        ).collect(Collectors.joining("\n"));
-        return BotMessage.simpleTextMessage(stringBuilder.append(body).toString());
+        String head = "咱可以帮你做这些事！\n";
+        String body = lineList.collect(Collectors.joining("\n"));
+
+        return BotMessage.simpleTextMessage(head + body);
     }
 }
