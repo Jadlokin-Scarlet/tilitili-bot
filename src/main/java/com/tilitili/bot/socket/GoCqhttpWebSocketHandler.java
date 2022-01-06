294357307038514fef845f24704e0157ed6ca24b
黄克凡 <w545459363@126.com>
2022-01-06 17:48:26 +0800

    message record

--- src/main/java/com/tilitili/bot/socket/GoCqhttpWebSocketHandler.java
+++ src/main/java/com/tilitili/bot/socket/GoCqhttpWebSocketHandler.java
@@ -2,6 +2,7 @@ package com.tilitili.bot.socket;
 
 import com.tilitili.bot.service.BotService;
 import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.manager.BotMessageRecordManager;
 import com.tilitili.common.manager.GoCqhttpManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,14 @@ import java.net.URISyntaxException;
 @Slf4j
 @Component
 public class GoCqhttpWebSocketHandler extends BaseWebSocketHandler {
-
+    private final BotMessageRecordManager botMessageRecordManager;
     private final GoCqhttpManager goCqhttpManager;
     private final BotService botService;
 
     @Autowired
-    public GoCqhttpWebSocketHandler(GoCqhttpManager goCqhttpManager, BotService botService) throws URISyntaxException {
+    public GoCqhttpWebSocketHandler(BotMessageRecordManager botMessageRecordManager, GoCqhttpManager goCqhttpManager, BotService botService) throws URISyntaxException {
         super(new URI(goCqhttpManager.getWebSocketUrl()));
+        this.botMessageRecordManager = botMessageRecordManager;
         this.goCqhttpManager = goCqhttpManager;
         this.botService = botService;
     }
@@ -30,6 +32,7 @@ public class GoCqhttpWebSocketHandler extends BaseWebSocketHandler {
         if (message.contains("meta_event_type\":\"heartbeat")) return;
         if (! message.contains("\"message_type\":\"guild\"")) return;
         BotMessage botMessage = goCqhttpManager.handleGoCqhttpWsMessageToBotMessage(message);
+        botMessageRecordManager.logRecord(message, botMessage);
         botService.syncHandleTextMessage(botMessage);
     }
 
--- src/main/java/com/tilitili/bot/socket/MiraiWebSocketHandler.java
+++ src/main/java/com/tilitili/bot/socket/MiraiWebSocketHandler.java
@@ -2,6 +2,7 @@ package com.tilitili.bot.socket;
 
 import com.tilitili.bot.service.BotService;
 import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.manager.BotMessageRecordManager;
 import com.tilitili.common.manager.MiraiManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,14 @@ import java.net.URISyntaxException;
 @Slf4j
 @Component
 public class MiraiWebSocketHandler extends BaseWebSocketHandler {
-
+    private final BotMessageRecordManager botMessageRecordManager;
     private final BotService botService;
     private final MiraiManager miraiManager;
 
     @Autowired
-    public MiraiWebSocketHandler(MiraiManager miraiManager, BotService botService) throws URISyntaxException {
+    public MiraiWebSocketHandler(BotMessageRecordManager botMessageRecordManager, MiraiManager miraiManager, BotService botService) throws URISyntaxException {
         super(new URI(miraiManager.getWebSocketUrl()));
+        this.botMessageRecordManager = botMessageRecordManager;
         this.miraiManager = miraiManager;
         this.botService = botService;
     }
@@ -28,6 +30,7 @@ public class MiraiWebSocketHandler extends BaseWebSocketHandler {
     public void handleTextMessage(String message) {
         log.debug("Message Received [{}]",message);
         BotMessage botMessage = miraiManager.handleMiraiWsMessageToBotMessage(message);
+        botMessageRecordManager.logRecord(message, botMessage);
         botService.syncHandleTextMessage(botMessage);
     }
 
