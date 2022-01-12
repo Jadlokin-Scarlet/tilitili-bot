34703e2019de3b3cbbd68439bfc8b1d62035b716
黄克凡 <w545459363@126.com>
2022-01-12 18:17:01 +0800

    botmc

--- src/main/java/com/tilitili/bot/service/mirai/McPingHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/McPingHandle.java
@@ -4,21 +4,26 @@ import com.tilitili.bot.emnus.MessageHandleEnum;
 import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.bot.service.BotSessionService;
 import com.tilitili.common.entity.view.bot.BotMessage;
-import com.tilitili.common.entity.view.bot.mcsrvstat.McsrvstatResponse;
-import com.tilitili.common.manager.McsrvstatManager;
+import com.tilitili.common.entity.view.bot.mcping.McPingMod;
+import com.tilitili.common.entity.view.bot.mcping.McPingResponse;
+import com.tilitili.common.manager.McPingManager;
 import com.tilitili.common.utils.Asserts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.stream.Collectors;
 
 @Component
 public class McPingHandle extends ExceptionRespMessageHandle {
 	private final String hostKey = "hostKey";
 
-	private final McsrvstatManager mcsrvstatManager;
+	private final McPingManager mcPingManager;
 
 	@Autowired
-	public McPingHandle(McsrvstatManager mcsrvstatManager) {
-		this.mcsrvstatManager = mcsrvstatManager;
+	public McPingHandle(McPingManager mcPingManager) {
+		this.mcPingManager = mcPingManager;
 	}
 
 	@Override
@@ -27,7 +32,7 @@ public class McPingHandle extends ExceptionRespMessageHandle {
 	}
 
 	@Override
-	public BotMessage handleMessage(BotMessageAction messageAction) {
+	public BotMessage handleMessage(BotMessageAction messageAction) throws IOException {
 		BotSessionService.MiraiSession session = messageAction.getSession();
 		String key = messageAction.getKey();
 		Asserts.notBlank(key, "key是啥");
@@ -37,16 +42,19 @@ public class McPingHandle extends ExceptionRespMessageHandle {
 			session.put(hostKey, value);
 			return BotMessage.simpleTextMessage("记住啦！下次mcp不用加地址了。");
 		}
-		String host = messageAction.getValueOrDefault(session.get(hostKey));
-		Asserts.notBlank(host, "没有地址");
-		McsrvstatResponse response = mcsrvstatManager.mcping(host);
-		Boolean online = response.getOnline();
-		Asserts.isTrue(online, "服务器不在线");
+		String url = messageAction.getValueOrDefault(session.get(hostKey));
+		Asserts.notBlank(url, "没有地址");
+		Asserts.isTrue(url.contains(":"), "地址格式不对");
+		String host = url.substring(0, url.indexOf(":"));
+		String port = url.substring(url.indexOf(":") + 1);
+		McPingResponse response = mcPingManager.mcPing(new InetSocketAddress(host, Integer.parseInt(port)));
+		Asserts.notNull(response, "服务器不在线");
 		Integer onlinePlayerCnt = response.getPlayers().getOnline();
-		String version = response.getVersion();
+		String version = response.getVersion().getName();
 		String message = String.format("服务器在线！版本%s, 在线玩家数：%s", version, onlinePlayerCnt);
 		if (key.equals("mcm")) {
-			message += String.format("\nmod列表：%s", String.join(",", response.getMods().getNames()));
+			String modListStr = response.getModinfo().getModList().stream().map(McPingMod::getModid).collect(Collectors.joining(","));
+			message += String.format("\nmod列表：%s", modListStr);
 		}
 		return BotMessage.simpleTextMessage(message);
 	}
