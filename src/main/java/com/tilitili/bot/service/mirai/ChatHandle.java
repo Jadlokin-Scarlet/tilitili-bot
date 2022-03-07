2bd61313fed2ebc90a2c9199efebe1cbbb57c167
黄克凡 <w545459363@126.com>
2022-03-07 14:30:33 +0800

    chat

---
+++ src/main/java/com/tilitili/bot/service/mirai/ChatHandle.java
@@ -0,0 +1,27 @@
+package com.tilitili.bot.service.mirai;
+
+import com.alibaba.fastjson.JSONObject;
+import com.tilitili.bot.entity.bot.BotMessageAction;
+import com.tilitili.bot.service.mirai.base.ExceptionRespMessageHandle;
+import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.utils.Asserts;
+import com.tilitili.common.utils.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ChatHandle extends ExceptionRespMessageHandle {
+	@Override
+	public BotMessage handleMessage(BotMessageAction messageAction) throws Exception {
+		String req = messageAction.getParamOrDefault("提问", messageAction.getValue());
+		Asserts.notBlank(req, "格式错啦(提问)");
+
+		String respStr = HttpClientUtil.httpGet("http://api.qingyunke.com/api.php?key=free&appid=0&msg=" + req);
+		JSONObject resp = JSONObject.parseObject(respStr);
+		log.debug("请求青云 req={} result={}", req, respStr);
+		Asserts.checkEquals(resp.getInteger("result"), 0, "不对劲");
+
+		return BotMessage.simpleTextMessage(resp.getString("content"));
+	}
+}
