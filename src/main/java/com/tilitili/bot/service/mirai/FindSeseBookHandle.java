1458cea2d51249c8edf1a3ddf2b93ab43ab49386
黄克凡 <w545459363@126.com>
2022-05-13 18:29:59 +0800

    sesebook

---
+++ src/main/java/com/tilitili/bot/service/mirai/FindSeseBookHandle.java
@@ -0,0 +1,35 @@
+package com.tilitili.bot.service.mirai;
+
+import com.tilitili.bot.entity.bot.BotMessageAction;
+import com.tilitili.bot.service.mirai.base.ExceptionRespMessageHandle;
+import com.tilitili.common.emnus.TaskReason;
+import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.entity.view.message.SimpleTask;
+import com.tilitili.common.manager.TaskManager;
+import com.tilitili.common.utils.Asserts;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class FindSeseBookHandle extends ExceptionRespMessageHandle {
+	private final TaskManager taskManager;
+
+	public FindSeseBookHandle(TaskManager taskManager) {
+		this.taskManager = taskManager;
+	}
+
+	@Override
+	public BotMessage handleMessage(BotMessageAction messageAction) {
+		List<String> imageList = messageAction.getImageList();
+		String messageId = messageAction.getMessageId();
+		Asserts.notBlank(messageId, "参数异常");
+		Asserts.notEmpty(imageList, "格式错啦(图片)");
+		String url = imageList.get(0);
+		Asserts.notBlank(url, "格式错啦(图片)");
+
+		taskManager.simpleSpiderVideo(new SimpleTask().setReason(TaskReason.SPIDER_SESE_BOOK.value).setValueList(Arrays.asList(url, messageId)));
+		return BotMessage.emptyMessage();
+	}
+}
