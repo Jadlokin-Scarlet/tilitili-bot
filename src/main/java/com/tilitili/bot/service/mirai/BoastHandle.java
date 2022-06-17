2d47de2b1c3131fc2c00452cae98c83b5e265538
黄克凡 <w545459363@126.com>
2022-06-17 16:27:00 +0800

    boast

--- src/main/java/com/tilitili/bot/service/mirai/BoastHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/BoastHandle.java
@@ -3,6 +3,7 @@ package com.tilitili.bot.service.mirai;
 import com.alibaba.fastjson.JSONPath;
 import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.bot.service.mirai.base.ExceptionRespMessageHandle;
+import com.tilitili.common.emnus.SendTypeEmum;
 import com.tilitili.common.entity.view.bot.BotMessage;
 import com.tilitili.common.entity.view.bot.BotMessageChain;
 import com.tilitili.common.utils.Asserts;
@@ -23,15 +24,22 @@ public class BoastHandle extends ExceptionRespMessageHandle {
 	@Override
 	public BotMessage handleMessage(BotMessageAction messageAction) {
 		String key = messageAction.getKeyWithoutPrefix();
+		boolean isOther = Arrays.asList("夸夸他", "kkt").contains(key);
 		List<Long> atList = messageAction.getAtList();
-		if (!atList.contains(Long.valueOf(BOT_QQ)) && !atList.contains(Long.valueOf(BOT_GUILD_QQ))) {
-			return null;
+		if (messageAction.getBotMessage().getSendType().equals(SendTypeEmum.FRIEND_MESSAGE_STR)) {
+			if (isOther) {
+				return null;
+			}
+		} else {
+			if (!atList.contains(Long.valueOf(BOT_QQ)) && !atList.contains(Long.valueOf(BOT_GUILD_QQ))) {
+				return null;
+			}
 		}
 		String result = HttpClientUtil.httpGet("https://api.shadiao.app/chp");
 		Asserts.notBlank(result, "网络异常");
 		String text = JSONPath.read(result, "$.data.text", String.class);
 		Asserts.notBlank(text, "网络异常");
-		if (Arrays.asList("夸夸他", "kkt").contains(key)) {
+		if (isOther) {
 			Long firstAt = atList.stream().filter(Predicate.isEqual(Long.valueOf(BOT_QQ)).or(Predicate.isEqual(BOT_GUILD_QQ)).negate()).findFirst().orElse(null);
 			Asserts.notNull(firstAt, "想我夸谁鸭");
 			return BotMessage.simpleListMessage(Arrays.asList(
