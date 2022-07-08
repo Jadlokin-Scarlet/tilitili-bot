4b48f003b0cbb86a2254d09e9cef7d1cfb93a906
黄克凡 <w545459363@126.com>
2022-07-08 16:31:33 +0800

    for

--- src/main/java/com/tilitili/bot/service/mirai/ForwardHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/ForwardHandle.java
@@ -4,9 +4,14 @@ import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.bot.service.mirai.base.BaseMessageHandleAdapt;
 import com.tilitili.common.entity.BotSender;
 import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.entity.view.bot.BotMessageChain;
 import com.tilitili.common.mapper.mysql.BotSenderMapper;
+import com.tilitili.common.utils.StreamUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -20,7 +25,9 @@ public class ForwardHandle extends BaseMessageHandleAdapt {
 	@Override
 	public BotMessage handleMessage(BotMessageAction messageAction) {
 		if (messageAction.getBotSender().getId() == 4370L) {
-			return BotMessage.simpleListMessage(messageAction.getBotMessage().getBotMessageChainList()).setSender(baGroup);
+			List<BotMessageChain> botMessageChainList = messageAction.getBotMessage().getBotMessageChainList().stream()
+					.filter(StreamUtil.isEqual(BotMessageChain::getType, BotMessage.MESSAGE_TYPE_IMAGE)).collect(Collectors.toList());
+			return BotMessage.simpleListMessage(botMessageChainList).setSender(baGroup);
 		}
 		return null;
 	}
