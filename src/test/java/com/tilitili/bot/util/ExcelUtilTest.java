910d2c50eba3bfc10a158f25c4482d371e7ab906
黄克凡 <w545459363@126.com>
2022-09-23 10:17:22 +0800

    21

--- src/test/java/com/tilitili/bot/util/ExcelUtilTest.java
+++ src/test/java/com/tilitili/bot/util/ExcelUtilTest.java
@@ -1,6 +1,7 @@
 package com.tilitili.bot.util;
 
 import com.tilitili.bot.StartApplication;
+import com.tilitili.bot.entity.ExcelResult;
 import com.tilitili.bot.entity.RandomTalkDTO;
 import com.tilitili.common.utils.Gsons;
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
 import java.io.File;
-import java.util.List;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -21,7 +20,7 @@ public class ExcelUtilTest {
 	@Test
 	public void getListFromExcel() {
 		File file = new File("/Users/admin/Documents/随机对话模板-测试.xlsx");
-		List<RandomTalkDTO> talkDTOList = ExcelUtil.getListFromExcel(file, RandomTalkDTO.class);
-		System.out.println(Gsons.toJson(talkDTOList));
+		ExcelResult<RandomTalkDTO> excelResult = ExcelUtil.getListFromExcel(file, RandomTalkDTO.class);
+		System.out.println(Gsons.toJson(excelResult));
 	}
 }
