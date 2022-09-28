846ddc2b8d1e577cee4a45e1993ea21ecc0f0193
黄克凡 <w545459363@126.com>
2022-09-28 18:34:27 +0800

    event

--- src/main/java/com/tilitili/bot/service/mirai/event/NudgeEventHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/event/NudgeEventHandle.java
@@ -7,8 +7,11 @@ import com.tilitili.common.entity.view.bot.mirai.event.NudgeEvent;
 import com.tilitili.common.entity.view.bot.mirai.event.NudgeSubject;
 import com.tilitili.common.manager.BotManager;
 import com.tilitili.common.mapper.mysql.BotSenderMapper;
+import com.tilitili.common.utils.Asserts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class NudgeEventHandle extends AutoEventHandle<NudgeEvent> {
@@ -25,14 +28,22 @@ public class NudgeEventHandle extends AutoEventHandle<NudgeEvent> {
 	@Override
 	public void handleEvent(NudgeEvent event) throws Exception {
 		NudgeSubject subject = event.getSubject();
+
 		BotSender botSender;
 		if ("Group".equals(subject.getKind())) {
 			botSender = botSenderMapper.getBotSenderByGroup(subject.getId());
 		} else {
 			botSender = botSenderMapper.getBotSenderByQq(subject.getId());
 		}
-		BotEmum bot = BotEmum.getByValue(botSender.getBot());
+		if (Objects.equals(event.getFromId(), botSender.getBot())) {
+			return;
+		}
+		if (!Objects.equals(event.getTarget(), botSender.getBot())) {
+			return;
+		}
 
+		BotEmum bot = BotEmum.getByValue(botSender.getBot());
+		Asserts.notNull(bot, "啊咧，不对劲");
 		botManager.sendNudge(bot, botSender, event.getFromId());
 	}
 }
