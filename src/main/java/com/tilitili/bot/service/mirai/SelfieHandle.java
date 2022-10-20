d1ee59b7e2abc7ce3177a7b430a7db53eec6309c
黄克凡 <w545459363@126.com>
2022-10-20 11:54:41 +0800

    selfie

--- src/main/java/com/tilitili/bot/service/mirai/SelfieHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/SelfieHandle.java
@@ -24,8 +24,9 @@ import java.util.Random;
 public class SelfieHandle extends ExceptionRespMessageHandle {
 	private final AiImageManager aiImageManager;
 	private final Random random;
-	private final List<String> tagList = Arrays.asList("{best quality},{{masterpiece}},{highres},original,extremely detailed 8K wallpaper,1girl,cat ears green eyes,(cirno),child,{an extremely delicate and beautiful}".split(","));
+	private final List<String> defaultTagList = Arrays.asList("{best quality},{{masterpiece}},{highres},original,extremely detailed 8K wallpaper,1girl,cat ears green eyes,(cirno),{an extremely delicate and beautiful}".split(","));
 	private final List<String> badTagList = Arrays.asList("lowres,bad anatomy,bad hands,text,error,missing fngers,extra digt,fewer digits,cropped,wort quality,low quality,normal quality,jpeg artifacts,signature,watermark,username,blurry,bad feet,r18,((nswf)),((nude))".split(","));
+	private final List<String> ageTagList = Arrays.asList("child", "little girl", "teens");
 
 	@Autowired
 	public SelfieHandle(AiImageManager aiImageManager) {
@@ -35,13 +36,16 @@ public class SelfieHandle extends ExceptionRespMessageHandle {
 
 	@Override
 	public BotMessage handleMessage(BotMessageAction messageAction) throws Exception {
+		int randomInt = random.nextInt(6);
+		List<String> tagList = new ArrayList<>(defaultTagList);
+		tagList.add(ageTagList.get(randomInt % ageTagList.size()));
 		List<String> imageList = aiImageManager.getAiImageByTagList(tagList, badTagList);
 		Asserts.notEmpty(imageList, "啊嘞，生成失败了");
 		if (imageList.size() > 1) {
 			imageList = imageList.subList(1, imageList.size());
 		}
 		List<BotMessageChain> respList = new ArrayList<>();
-		if (random.nextInt(2) == 0) {
+		if ((randomInt & 1) == 0) {
 			respList.add(BotMessageChain.ofPlain("就，，就这一次哦。"));
 		} else {
 			respList.add(BotMessageChain.ofPlain("好，，好吧。"));
