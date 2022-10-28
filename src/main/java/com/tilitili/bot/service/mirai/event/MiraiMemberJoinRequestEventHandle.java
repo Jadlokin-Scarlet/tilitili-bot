7e8a28579283dc3729959c417af3bea745308907
黄克凡 <w545459363@126.com>
2022-10-28 10:16:02 +0800

    fix

--- src/main/java/com/tilitili/bot/service/mirai/event/MiraiMemberJoinRequestEventHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/event/MiraiMemberJoinRequestEventHandle.java
@@ -4,19 +4,16 @@ import com.tilitili.bot.service.BotSessionService;
 import com.tilitili.bot.service.mirai.base.MiraiAutoEventHandle;
 import com.tilitili.common.emnus.BotEmum;
 import com.tilitili.common.entity.BotSender;
-import com.tilitili.common.entity.BotSenderTaskMapping;
 import com.tilitili.common.entity.view.bot.BotMessage;
 import com.tilitili.common.entity.view.bot.mirai.event.MiraiMemberJoinRequestEvent;
 import com.tilitili.common.manager.BotManager;
+import com.tilitili.common.manager.BotSenderTaskMappingManager;
 import com.tilitili.common.mapper.mysql.BotSenderMapper;
 import com.tilitili.common.mapper.mysql.BotSenderTaskMappingMapper;
 import com.tilitili.common.utils.Asserts;
 import com.tilitili.common.utils.Gsons;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.function.Predicate;
 
 @Component
 public class MiraiMemberJoinRequestEventHandle extends MiraiAutoEventHandle<MiraiMemberJoinRequestEvent> {
@@ -25,24 +22,26 @@ public class MiraiMemberJoinRequestEventHandle extends MiraiAutoEventHandle<Mira
 	private final BotSenderMapper botSenderMapper;
 	private final BotSessionService botSessionService;
 	private final BotSenderTaskMappingMapper botSenderTaskMappingMapper;
+	private final BotSenderTaskMappingManager botSenderTaskMappingManager;
 
 	@Autowired
-	public MiraiMemberJoinRequestEventHandle(BotManager botManager, BotSenderMapper botSenderMapper, BotSessionService botSessionService, BotSenderTaskMappingMapper botSenderTaskMappingMapper) {
+	public MiraiMemberJoinRequestEventHandle(BotManager botManager, BotSenderMapper botSenderMapper, BotSessionService botSessionService, BotSenderTaskMappingMapper botSenderTaskMappingMapper, BotSenderTaskMappingManager botSenderTaskMappingManager) {
 		super(MiraiMemberJoinRequestEvent.class);
 		this.botManager = botManager;
 		this.botSenderMapper = botSenderMapper;
 		this.botSessionService = botSessionService;
 		this.botSenderTaskMappingMapper = botSenderTaskMappingMapper;
+		this.botSenderTaskMappingManager = botSenderTaskMappingManager;
 	}
 
 	@Override
 	public void handleEvent(BotEmum bot, MiraiMemberJoinRequestEvent event) {
-		String message = String.format("[%s][%s]申请加入[%s][%s]，是否接受(同意加群申请/拒绝加群申请)", event.getNick(), event.getFromId(), event.getGroupName(), event.getGroupId());
+		String message = String.format("[%s][%s]申请加入[%s][%s]，备注[%s]，是否接受(同意加群申请/拒绝加群申请)", event.getNick(), event.getFromId(), event.getGroupName(), event.getGroupId(), event.getMessage());
 		BotSender botSender = botSenderMapper.getBotSenderByGroup(event.getGroupId());
+		Asserts.checkEquals(bot.qq, botSender.getQq(), "没有权限");
 
 		// 校验权限
-		List<BotSenderTaskMapping> botSenderTaskMappingList = botSenderTaskMappingMapper.getBotSenderTaskMappingBySenderId(botSender.getId());
-		boolean hasEventHandle = botSenderTaskMappingList.stream().map(BotSenderTaskMapping::getTaskId).anyMatch(Predicate.isEqual(50L));
+		boolean hasEventHandle = botSenderTaskMappingManager.checkSenderHasTask(botSender.getId(), 50L);
 		Asserts.isTrue(hasEventHandle, "啊嘞，不对劲。");
 
 		BotSessionService.MiraiSession session = botSessionService.getSession(botSessionService.getSessionKey(botSender));
---
+++ src/main/java/com/tilitili/bot/service/mirai/event/MiraiMemberLeaveEventQuitHandle.java
@@ -0,0 +1,35 @@
+package com.tilitili.bot.service.mirai.event;
+
+import com.tilitili.bot.service.mirai.base.MiraiAutoEventHandle;
+import com.tilitili.common.emnus.BotEmum;
+import com.tilitili.common.entity.BotSender;
+import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.entity.view.bot.mirai.event.MiraiMemberLeaveEventQuit;
+import com.tilitili.common.manager.BotManager;
+import com.tilitili.common.mapper.mysql.BotSenderMapper;
+import com.tilitili.common.utils.Asserts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MiraiMemberLeaveEventQuitHandle extends MiraiAutoEventHandle<MiraiMemberLeaveEventQuit> {
+	private final BotManager botManager;
+	private final BotSenderMapper botSenderMapper;
+
+	@Autowired
+	public MiraiMemberLeaveEventQuitHandle(BotManager botManager, BotSenderMapper botSenderMapper) {
+		super(MiraiMemberLeaveEventQuit.class);
+		this.botManager = botManager;
+		this.botSenderMapper = botSenderMapper;
+	}
+
+	@Override
+	public void handleEvent(BotEmum bot, MiraiMemberLeaveEventQuit event) {
+		String message = String.format("%s离开了。", event.getMember().getMemberName());
+
+		BotSender botSender = botSenderMapper.getBotSenderByGroup(event.getMember().getGroup().getId());
+		Asserts.checkEquals(bot.qq, botSender.getQq(), "没有权限");
+
+		botManager.sendMessage(BotMessage.simpleTextMessage(message).setSenderId(botSender.getId()));
+	}
+}
