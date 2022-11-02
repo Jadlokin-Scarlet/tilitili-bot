023805f5ce9a848839aa8a274ff971f8defbc1a7
黄克凡 <w545459363@126.com>
2022-11-02 15:04:59 +0800

    boast

--- src/main/java/com/tilitili/bot/service/mirai/BoastHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/BoastHandle.java
@@ -7,6 +7,7 @@ import com.tilitili.common.entity.BotBoast;
 import com.tilitili.common.entity.query.BotBoastQuery;
 import com.tilitili.common.entity.view.bot.BotMessage;
 import com.tilitili.common.entity.view.bot.BotMessageChain;
+import com.tilitili.common.exception.AssertException;
 import com.tilitili.common.mapper.mysql.BotBoastMapper;
 import com.tilitili.common.utils.Asserts;
 import com.tilitili.common.utils.HttpClientUtil;
@@ -33,20 +34,30 @@ public class BoastHandle extends ExceptionRespMessageHandle {
 	public BotMessage handleMessage(BotMessageAction messageAction) {
 		Long botQQ = messageAction.getBotSender().getBot();
 		String key = messageAction.getKeyWithoutPrefix();
-		boolean isOther = Arrays.asList("夸夸他", "kkt").contains(key);
 		List<Long> atList = messageAction.getAtList();
-		String result = HttpClientUtil.httpGet("https://api.shadiao.app/chp");
-		Asserts.notBlank(result, "网络异常");
-		String text = JSONPath.read(result, "$.data.text", String.class);
+		String text;
+		String type;
+		if (Arrays.asList("夸夸他", "kkt", "夸夸我", "kkw").contains(key)) {
+			String result = HttpClientUtil.httpGet("https://api.shadiao.app/chp");
+			Asserts.notBlank(result, "网络异常");
+			text = JSONPath.read(result, "$.data.text", String.class);
+			type = "夸夸他";
+		} else if (Arrays.asList("骂骂我", "mmw", "骂骂他", "mmt").contains(key)){
+			text = HttpClientUtil.httpGet("https://nmsl.yfchat.xyz/api.php?level=min");
+			type = "骂骂他";
+		} else {
+			throw new AssertException();
+		}
 		Asserts.notBlank(text, "网络异常");
 
-		if (botBoastMapper.getBotBoastByCondition(new BotBoastQuery().setText(text)).isEmpty()) {
-			botBoastMapper.addBotBoastSelective(new BotBoast().setText(text));
+		if (botBoastMapper.getBotBoastByCondition(new BotBoastQuery().setText(text).setType(type)).isEmpty()) {
+			botBoastMapper.addBotBoastSelective(new BotBoast().setText(text).setType(type));
 		}
 
+		boolean isOther = Arrays.asList("夸夸他", "kkt", "骂骂他", "mmt").contains(key);
 		if (isOther) {
 			Long firstAt = atList.stream().filter(Predicate.isEqual(botQQ).or(Predicate.isEqual(BOT_GUILD_QQ)).negate()).findFirst().orElse(null);
-			Asserts.notNull(firstAt, "想我夸谁鸭");
+			Asserts.notNull(firstAt, "谁？");
 			return BotMessage.simpleListMessage(Arrays.asList(
 					BotMessageChain.ofAt(firstAt),
 					BotMessageChain.ofPlain(" "),
