b06b8e5abbdaeec3e968d90921cfc29783f1c789
黄克凡 <w545459363@126.com>
2022-11-03 19:00:48 +0800

    score

--- src/main/java/com/tilitili/bot/service/mirai/SignHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/SignHandle.java
@@ -66,7 +66,7 @@ public class SignHandle extends ExceptionRespMessageHandle {
 	}
 
 	private BotMessage handleQueryRankMessage(BotMessageAction messageAction) {
-		List<BotUserRankDTO> rankDTOList = botUserItemMappingMapper.getBotUserScoreRank(10);
+		List<BotUserRankDTO> rankDTOList = botUserItemMappingMapper.getBotUserScoreRank(5);
 //		List<BotUser> userList = botUserMapper.getBotUserByCondition(new BotUserQuery().setStatus(0).setSorter("score").setSorted("desc").setPageSize(10));
 //		if (userList.size() > 10) userList = userList.subList(0, 10);
 
