36ebfdf3cf6ee5c36a37a0764bd1c3c9d9adb279
黄克凡 <w545459363@126.com>
2022-11-09 16:23:01 +0800

    group admin

---
+++ src/main/java/com/tilitili/bot/service/mirai/GroupAdminHandle.java
@@ -0,0 +1,54 @@
+package com.tilitili.bot.service.mirai;
+
+import com.tilitili.bot.entity.bot.BotMessageAction;
+import com.tilitili.bot.service.mirai.base.ExceptionRespMessageHandle;
+import com.tilitili.common.emnus.BotEmum;
+import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.manager.BotManager;
+import com.tilitili.common.utils.Asserts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GroupAdminHandle extends ExceptionRespMessageHandle {
+	private final BotManager botManager;
+
+	@Autowired
+	public GroupAdminHandle(BotManager botManager) {
+		this.botManager = botManager;
+	}
+
+	@Override
+	public BotMessage handleMessage(BotMessageAction messageAction) throws Exception {
+		String key = messageAction.getKey();
+		switch (key) {
+			case "管理员": return handleAdmin(messageAction);
+			case "取消管理员": return handleDeleteAdmin(messageAction);
+		}
+		return null;
+	}
+
+	private BotMessage handleDeleteAdmin(BotMessageAction messageAction) {
+		BotEmum bot = messageAction.getBot();
+		List<Long> atList = messageAction.getAtList();
+		Long group = messageAction.getBotSender().getGroup();
+		Asserts.notEmpty(atList, "谁?");
+		for (Long qq : atList) {
+			botManager.setMemberAdmin(bot, group, qq, true);
+		}
+		return BotMessage.simpleTextMessage("好了喵。");
+	}
+
+	private BotMessage handleAdmin(BotMessageAction messageAction) {
+		BotEmum bot = messageAction.getBot();
+		List<Long> atList = messageAction.getAtList();
+		Long group = messageAction.getBotSender().getGroup();
+		Asserts.notEmpty(atList, "谁?");
+		for (Long qq : atList) {
+			botManager.setMemberAdmin(bot, group, qq, false);
+		}
+		return BotMessage.simpleTextMessage("好了喵。");
+	}
+}
