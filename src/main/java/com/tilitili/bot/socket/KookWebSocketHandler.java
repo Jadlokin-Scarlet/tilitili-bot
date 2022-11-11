2c73b96aac5db412865426ffb094d407acda1061
黄克凡 <w545459363@126.com>
2022-11-11 17:46:53 +0800

    kook

--- src/main/java/com/tilitili/bot/socket/KookWebSocketHandler.java
+++ src/main/java/com/tilitili/bot/socket/KookWebSocketHandler.java
@@ -15,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class KookWebSocketHandler extends BotWebSocketHandler {
     private final ScheduledExecutorService executorService;
+    private int sn = 0;
 
     public KookWebSocketHandler(URI serverUri, BotEmum bot, BotService botService, BotManager botManager) {
         super(serverUri, bot, botService, botManager);
@@ -26,8 +27,9 @@ public class KookWebSocketHandler extends BotWebSocketHandler {
         log.debug("Message Received message={}", message);
         KookWsData<?> kookWsData = Gsons.fromJson(message, KookWsData.class);
         switch (kookWsData.getS()) {
-            case 0: botService.syncHandleTextMessage(message, bot); break;
-            case 1: case 3: executorService.schedule(() -> this.send("{\"s\": 2,\"sn\": 0}"), 30, TimeUnit.SECONDS); break;
+            case 0: sn = kookWsData.getSn() == null? 0: kookWsData.getSn();
+                botService.syncHandleTextMessage(message, bot); break;
+            case 1: case 3: executorService.schedule(() -> this.send("{\"s\": 2,\"sn\": "+sn+"}"), 30, TimeUnit.SECONDS); break;
             default: log.warn("记录s="+kookWsData.getS());
         }
     }
