129209c538f4a3e50e75b58f085ce50598996bb5
黄克凡 <w545459363@126.com>
2022-11-14 18:26:04 +0800

    kook

--- src/main/java/com/tilitili/bot/service/mirai/base/KookAutoEventHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/base/KookAutoEventHandle.java
@@ -2,13 +2,14 @@ package com.tilitili.bot.service.mirai.base;
 
 import com.google.gson.reflect.TypeToken;
 import com.tilitili.common.emnus.BotEmum;
+import com.tilitili.common.entity.view.bot.kook.KookEventResponse;
 import com.tilitili.common.utils.Gsons;
 
 public abstract class KookAutoEventHandle<T> implements BaseEventHandle {
 	private final TypeToken<?> type;
 
 	public KookAutoEventHandle(Class<T> clazz) {
-		this.type = TypeToken.getParameterized(clazz);
+		this.type = TypeToken.getParameterized(KookEventResponse.class, clazz);
 	}
 
 	@Override
--- src/main/java/com/tilitili/bot/service/mirai/event/KookJoinedChannelHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/event/KookJoinedChannelHandle.java
@@ -3,8 +3,11 @@ package com.tilitili.bot.service.mirai.event;
 import com.tilitili.bot.service.mirai.base.KookAutoEventHandle;
 import com.tilitili.common.emnus.BotEmum;
 import com.tilitili.common.entity.view.bot.mirai.event.KookJoinedChannel;
+import com.tilitili.common.utils.Gsons;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class KookJoinedChannelHandle extends KookAutoEventHandle<KookJoinedChannel> {
 	public KookJoinedChannelHandle() {
@@ -13,6 +16,7 @@ public class KookJoinedChannelHandle extends KookAutoEventHandle<KookJoinedChann
 
 	@Override
 	public void handleEvent(BotEmum bot, KookJoinedChannel event) throws Exception {
+		log.info(Gsons.toJson(event));
 //		event.getD().getExtra().get
 	}
 }
