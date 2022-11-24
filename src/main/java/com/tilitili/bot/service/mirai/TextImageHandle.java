a3037f8c5ed3715903f34d1990e13015b08ecb65
黄克凡 <w545459363@126.com>
2022-11-24 18:20:21 +0800

    image

--- src/main/java/com/tilitili/bot/service/mirai/TextImageHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/TextImageHandle.java
@@ -1,7 +1,7 @@
 package com.tilitili.bot.service.mirai;
 
 import com.tilitili.bot.entity.bot.BotMessageAction;
-import com.tilitili.bot.service.TemplateImageService;
+import com.tilitili.common.manager.TemplateImageManager;
 import com.tilitili.bot.service.mirai.base.BaseMessageHandleAdapt;
 import com.tilitili.common.entity.view.bot.BotMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +9,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TextImageHandle extends BaseMessageHandleAdapt {
-	private final TemplateImageService templateImageService;
+	private final TemplateImageManager templateImageManager;
 
 	@Autowired
-	public TextImageHandle(TemplateImageService templateImageService) {
-		this.templateImageService = templateImageService;
+	public TextImageHandle(TemplateImageManager templateImageManager) {
+		this.templateImageManager = templateImageManager;
 	}
 
 	@Override
 	public BotMessage handleMessage(BotMessageAction messageAction) throws Exception {
 		String text = messageAction.getBody();
-		String imageUrl = templateImageService.getLongStringImage(text);
+		String imageUrl = templateImageManager.getLongStringImage(text);
 		return BotMessage.simpleImageMessage(imageUrl);
 	}
 }
