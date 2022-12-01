99147e6530443b2688ae1a24b9eb8792642a780c
黄克凡 <w545459363@126.com>
2022-12-01 10:06:21 +0800

    favorite

--- src/main/java/com/tilitili/bot/service/mirai/FavoriteHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/FavoriteHandle.java
@@ -6,27 +6,39 @@ import com.tilitili.common.constant.BotUserConstant;
 import com.tilitili.common.emnus.FavoriteEmum;
 import com.tilitili.common.emnus.SendTypeEmum;
 import com.tilitili.common.entity.BotFavorite;
+import com.tilitili.common.entity.BotFavoriteActionAdd;
 import com.tilitili.common.entity.BotFavoriteTalk;
 import com.tilitili.common.entity.BotSender;
 import com.tilitili.common.entity.dto.BotUserDTO;
 import com.tilitili.common.entity.query.BotFavoriteTalkQuery;
 import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.entity.view.bot.BotMessageChain;
+import com.tilitili.common.manager.BotFavoriteManager;
+import com.tilitili.common.mapper.mysql.BotFavoriteActionAddMapper;
 import com.tilitili.common.mapper.mysql.BotFavoriteMapper;
 import com.tilitili.common.mapper.mysql.BotFavoriteTalkMapper;
+import com.tilitili.common.utils.DateUtils;
+import com.tilitili.common.utils.RedisCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class FavoriteHandle extends BaseMessageHandleAdapt {
 	private final Random random;
+	private final RedisCache redisCache;
 	private final BotFavoriteMapper botFavoriteMapper;
+	private final BotFavoriteManager botFavoriteManager;
 	private final BotFavoriteTalkMapper botFavoriteTalkMapper;
+	private final BotFavoriteActionAddMapper botFavoriteActionAddMapper;
 
 	@Autowired
-	public FavoriteHandle(BotFavoriteMapper botFavoriteMapper, BotFavoriteTalkMapper botFavoriteTalkMapper) {
+	public FavoriteHandle(RedisCache redisCache, BotFavoriteMapper botFavoriteMapper, BotFavoriteManager botFavoriteManager, BotFavoriteTalkMapper botFavoriteTalkMapper, BotFavoriteActionAddMapper botFavoriteActionAddMapper) {
+		this.redisCache = redisCache;
+		this.botFavoriteManager = botFavoriteManager;
+		this.botFavoriteActionAddMapper = botFavoriteActionAddMapper;
 		this.random = new Random(System.currentTimeMillis());
 		this.botFavoriteMapper = botFavoriteMapper;
 		this.botFavoriteTalkMapper = botFavoriteTalkMapper;
@@ -36,7 +48,12 @@ public class FavoriteHandle extends BaseMessageHandleAdapt {
 	public BotMessage handleMessage(BotMessageAction messageAction) throws Exception {
 		BotUserDTO botUser = messageAction.getBotUser();
 		BotSender botSender = messageAction.getBotSender();
-		String text = messageAction.getText();
+		String action = messageAction.getText();
+
+		// 触发对话的关键词不会太长
+		if (action.length() > 5) {
+			return null;
+		}
 
 		// 其他平台必须绑定主账号
 		if (BotUserConstant.USER_TYPE_QQ != botUser.getType()) {
@@ -46,7 +63,7 @@ public class FavoriteHandle extends BaseMessageHandleAdapt {
 		Long userId = botUser.getId();
 
 		// 解锁好感度条件
-		if ("你好".equals(text)) {
+		if ("你好".equals(action)) {
 			List<Long> atList = messageAction.getAtList();
 			atList.retainAll(BotUserConstant.BOT_USER_ID_LIST);
 			boolean hasAtBot = !atList.isEmpty();
@@ -63,22 +80,40 @@ public class FavoriteHandle extends BaseMessageHandleAdapt {
 		}
 
 		// 没解锁的跳过判定
-		BotFavorite favorite = botFavoriteMapper.getBotFavoriteByUserId(userId);
-		if (favorite == null) {
+		BotFavorite botFavorite = botFavoriteMapper.getBotFavoriteByUserId(userId);
+		if (botFavorite == null) {
 			return null;
 		}
+		String level = botFavorite.getLevel();
 
-		// 触发对话的关键词不会太长
-		if (text.length() > 5) {
-			return null;
-		}
-
-		List<BotFavoriteTalk> favoriteTalkList = botFavoriteTalkMapper.getBotFavoriteTalkByCondition(new BotFavoriteTalkQuery().setAction(text).setLevel(favorite.getLevel()));
+		// 获取对话
+		List<BotFavoriteTalk> favoriteTalkList = botFavoriteTalkMapper.getBotFavoriteTalkByCondition(new BotFavoriteTalkQuery().setAction(action).setLevel(level));
 		if (favoriteTalkList.isEmpty()) {
 			return null;
 		}
 
+		List<BotMessageChain> respChainList = new ArrayList<>();
+
 		BotFavoriteTalk favoriteTalk = favoriteTalkList.get(random.nextInt(favoriteTalkList.size()));
-		return BotMessage.simpleTextMessage(favoriteTalk.getResp());
+		respChainList.add(BotMessageChain.ofPlain(favoriteTalk.getResp()));
+
+		// 获取好感度增量
+		BotFavoriteActionAdd favoriteActionAdd = botFavoriteActionAddMapper.getBotFavoriteActionAddByActionAndLevel(action, level);
+		if (favoriteActionAdd != null) {
+			// 凌晨4点刷新
+			String dayStr = DateUtils.formatDateYMD(DateUtils.addTime(new Date(), Calendar.HOUR_OF_DAY, -4));
+			// 每个人每天每个动作只能加一次好感度
+			String redisKey = String.format("favorite-%s-%s-%s", dayStr, userId, action);
+			if (!redisCache.exists(redisKey)) {
+				Integer addFavorite = botFavoriteManager.safeAddFavorite(userId, favoriteActionAdd.getFavorite());
+				if (addFavorite != 0) {
+					respChainList.add(BotMessageChain.ofPlain(String.format("(好感度+%d)", addFavorite)));
+					redisCache.setValue(redisKey, "yes", Math.toIntExact(TimeUnit.DAYS.toMillis(1)));
+				}
+			}
+		}
+
+
+		return BotMessage.simpleListMessage(respChainList);
 	}
 }
