8ee33d007272dff7e0b7718ff305f22159e9f9b5
黄克凡 <w545459363@126.com>
2022-12-14 17:07:46 +0800

    help

--- src/main/java/com/tilitili/bot/service/mirai/HelpHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/HelpHandle.java
@@ -2,30 +2,82 @@ package com.tilitili.bot.service.mirai;
 
 import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.bot.service.mirai.base.ExceptionRespMessageHandle;
+import com.tilitili.common.constant.BotUserConstant;
 import com.tilitili.common.entity.BotSender;
+import com.tilitili.common.entity.BotSenderTaskMapping;
 import com.tilitili.common.entity.BotTask;
 import com.tilitili.common.entity.dto.BotTaskDTO;
 import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.mapper.mysql.BotSenderTaskMappingMapper;
 import com.tilitili.common.mapper.mysql.BotTaskMapper;
 import com.tilitili.common.utils.Asserts;
 import org.jsoup.helper.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class HelpHandle extends ExceptionRespMessageHandle {
     private final BotTaskMapper botTaskMapper;
+    private final BotSenderTaskMappingMapper botSenderTaskMappingMapper;
 
     @Autowired
-    public HelpHandle(BotTaskMapper botTaskMapper) {
+    public HelpHandle(BotTaskMapper botTaskMapper, BotSenderTaskMappingMapper botSenderTaskMappingMapper) {
         this.botTaskMapper = botTaskMapper;
+        this.botSenderTaskMappingMapper = botSenderTaskMappingMapper;
     }
 
     @Override
     public BotMessage handleMessage(BotMessageAction messageAction) {
-        String paramListStr = messageAction.getValueOrDefault("").replaceAll("\\s+", " ").trim();;
+        switch (messageAction.getKeyWithoutPrefix()) {
+            case "启用": return handleOpen(messageAction);
+            case "禁用": return handleClose(messageAction);
+            default: return handelHelp(messageAction);
+        }
+    }
+
+    private BotMessage handleClose(BotMessageAction messageAction) {
+        if (!BotUserConstant.MASTER_USER_ID.equals(messageAction.getBotUser().getId())) {
+            return null;
+        }
+        Long senderId = messageAction.getBotSender().getId();
+        String taskNameList = messageAction.getValue();
+        Asserts.notBlank(taskNameList, "格式错啦");
+        List<Long> taskIdList = Arrays.stream(taskNameList.split("，")).map(botTaskMapper::getBotTaskByName).map(BotTask::getId).collect(Collectors.toList());
+
+        for (Long taskId : taskIdList) {
+            BotSenderTaskMapping botSenderTaskMapping = botSenderTaskMappingMapper.getBotSenderTaskMappingBySenderIdAndTaskId(senderId, taskId);
+            if (botSenderTaskMapping != null) {
+                botSenderTaskMappingMapper.deleteBotSenderTaskMappingById(botSenderTaskMapping.getId());
+            }
+        }
+        return BotMessage.simpleTextMessage("√");
+    }
+
+    private BotMessage handleOpen(BotMessageAction messageAction) {
+        if (!BotUserConstant.MASTER_USER_ID.equals(messageAction.getBotUser().getId())) {
+            return null;
+        }
+        Long senderId = messageAction.getBotSender().getId();
+        String taskNameList = messageAction.getValue();
+        Asserts.notBlank(taskNameList, "格式错啦");
+        List<Long> taskIdList = Arrays.stream(taskNameList.split("，")).map(botTaskMapper::getBotTaskByName).map(BotTask::getId).collect(Collectors.toList());
+
+        for (Long taskId : taskIdList) {
+            BotSenderTaskMapping botSenderTaskMapping = botSenderTaskMappingMapper.getBotSenderTaskMappingBySenderIdAndTaskId(senderId, taskId);
+            if (botSenderTaskMapping == null) {
+                botSenderTaskMappingMapper.addBotSenderTaskMappingSelective(new BotSenderTaskMapping().setTaskId(taskId).setSenderId(senderId));
+            }
+        }
+        return BotMessage.simpleTextMessage("√");
+    }
+
+    private BotMessage handelHelp(BotMessageAction messageAction) {
+        String paramListStr = messageAction.getValueOrDefault("").replaceAll("\\s+", " ").trim();
+        ;
 //        String sendType = messageAction.getBotMessage().getSendType();
 //        String guildPrefix = sendType.equals(SendTypeEmum.Guild_Message.sendType)? ".": "";
 
@@ -51,7 +103,8 @@ public class HelpHandle extends ExceptionRespMessageHandle {
             Asserts.isTrue(botTaskList.size() < 2, "不对劲");
             String reply;
             if (botTaskList.isEmpty()) {
-                reply = String.format("[%s]的作用是[%s]。", paramListStr, paramListStr);
+                return null;
+//                reply = String.format("[%s]的作用是[%s]。", paramListStr, paramListStr);
             } else {
                 BotTask botTask = botTaskList.get(0);
                 reply = String.format("[%s]的作用是[%s]。", paramListStr, botTask.getDescription());
