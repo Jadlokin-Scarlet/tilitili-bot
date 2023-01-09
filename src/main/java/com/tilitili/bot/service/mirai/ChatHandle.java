a6e6dea3f8a1753afeb4e7600a40a0bc4604691c
黄克凡 <w545459363@126.com>
2023-01-09 16:09:19 +0800

    chat

--- src/main/java/com/tilitili/bot/service/mirai/ChatHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/ChatHandle.java
@@ -121,7 +121,7 @@ public class ChatHandle extends ExceptionRespMessageHandle {
 					if (StringUtils.isNotBlank(reply) && !reply.contains("小龙女")) break;
 				}
 				if (StringUtils.isBlank(reply) || reply.contains("小龙女")) return BotMessage.simpleTextMessage("网络似乎不太通常呢。");
-				if (isRandomReply && reply.contains("风太大了，没有听清，能再说一遍么")) {
+				if (isRandomReply && (reply.contains("能再说一遍么") || reply.contains("不是姑姑我不明白，这世界变化快"))) {
 					return BotMessage.emptyMessage();
 				}
 				break;
