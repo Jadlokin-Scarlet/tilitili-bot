76abf24e8ff4756cfe769787825e857c45efb9da
z-ero <w545459363@126.com>
2023-01-30 16:14:47 +0800

    event

--- src/main/java/com/tilitili/bot/service/mirai/ForwardHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/ForwardHandle.java
@@ -102,7 +102,7 @@ public class ForwardHandle extends BaseMessageHandleAdapt {
 			String userName = messageAction.getBotUser().getName();
 
 			List<BotMessageChain> newMessageChainList = new ArrayList<>();
-			newMessageChainList.add(BotMessageChain.ofPlain(String.format("%s%s：", sourceNameStr, userName)));
+			newMessageChainList.add(BotMessageChain.ofPlain(String.format("%s<%s> ", sourceNameStr, userName)));
 			newMessageChainList.addAll(sourceMessageChainList);
 			return BotMessage.simpleListMessage(newMessageChainList).setSenderId(targetSenderId);
 		}
