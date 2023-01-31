ca71a1a384a2099c9c0d6920e906a1e2881fbdf5
z-ero <w545459363@126.com>
2023-01-31 16:51:10 +0800

    mc

--- src/test/java/com/tilitili/bot/service/BotServiceTest.java
+++ src/test/java/com/tilitili/bot/service/BotServiceTest.java
@@ -18,10 +18,6 @@ public class BotServiceTest {
 	BotService tester;
 
 	@Test
-	public void syncHandleEvent() {
-	}
-
-	@Test
 	public void syncHandleTextMessage() {
 		tester.testHandleMessage(BotEnum.MINECRAFT, "{\"player\":{\"uuid\":\"34e35969-8c80-37c9-9257-57c7672ccfbe\",\"displayName\":\"§3§k1§5帅帅§3§k1§r\",\"port\":4498,\"exhaustion\":0.02523899,\"exp\":0.42857128,\"whitelisted\":false,\"banned\":false,\"op\":true,\"balance\":0.0},\"playerName\":\"§3§k1§5帅帅§3§k1§r\",\"message\":\"kkt TachibanaHinan0\",\"eventType\":\"PlayerChat\"}");
 	}
