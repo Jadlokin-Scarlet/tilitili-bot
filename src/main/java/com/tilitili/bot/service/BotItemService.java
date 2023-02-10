d2e1e22b96090e65655599d5ca8a5f1dc5ad3a8a
z-ero <w545459363@126.com>
2023-02-10 19:25:06 +0800

    admin

--- src/main/java/com/tilitili/bot/service/BotItemService.java
+++ src/main/java/com/tilitili/bot/service/BotItemService.java
@@ -3,8 +3,10 @@ package com.tilitili.bot.service;
 import com.tilitili.bot.service.item.BaseItem;
 import com.tilitili.common.entity.BotItem;
 import com.tilitili.common.entity.BotSender;
+import com.tilitili.common.entity.BotUserItemMapping;
 import com.tilitili.common.entity.dto.BotUserDTO;
 import com.tilitili.common.exception.AssertException;
+import com.tilitili.common.mapper.mysql.BotUserItemMappingMapper;
 import com.tilitili.common.utils.Asserts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +19,12 @@ import java.util.Map;
 @Slf4j
 @Service
 public class BotItemService {
+	private final BotUserItemMappingMapper botUserItemMappingMapper;
 	private final Map<String, BaseItem> itemMap;
 
 	@Autowired
-	public BotItemService(List<BaseItem> itemList) {
+	public BotItemService(List<BaseItem> itemList, BotUserItemMappingMapper botUserItemMappingMapper) {
+		this.botUserItemMappingMapper = botUserItemMappingMapper;
 		this.itemMap = new HashMap<>();
 		for (BaseItem baseItem : itemList) {
 			this.itemMap.put(baseItem.getName(), baseItem);
@@ -30,6 +34,9 @@ public class BotItemService {
 	public boolean useItem(BotSender botSender, BotUserDTO botUser, BotItem botItem) {
 		BaseItem item = itemMap.get(botItem.getName());
 		Asserts.notNull(item, "这个用不了吧。");
+		Long userId = botUser.getId();
+		BotUserItemMapping botUserItemMapping = botUserItemMappingMapper.getBotUserItemMappingByUserIdAndItemId(userId, botItem.getId());
+		Asserts.notNull(botUserItemMapping, "你还没拥有他");
 		return item.useItem(botSender, botUser, botItem);
 	}
 
--- src/main/java/com/tilitili/bot/service/item/Divorce.java
+++ src/main/java/com/tilitili/bot/service/item/Divorce.java
@@ -1,40 +1,43 @@
 package com.tilitili.bot.service.item;
 
+import com.tilitili.common.entity.BotFavorite;
 import com.tilitili.common.entity.BotItem;
 import com.tilitili.common.entity.BotSender;
 import com.tilitili.common.entity.BotUserItemMapping;
 import com.tilitili.common.entity.dto.BotUserDTO;
 import com.tilitili.common.manager.BotUserItemMappingManager;
-import com.tilitili.common.mapper.mysql.BotUserItemMappingMapper;
+import com.tilitili.common.mapper.mysql.BotFavoriteMapper;
 import com.tilitili.common.utils.Asserts;
 import com.tilitili.common.utils.RedisCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Viagra extends BaseItem {
+public class Divorce extends BaseItem {
 	private final BotUserItemMappingManager botUserItemMappingManager;
-	private final RedisCache redisCache;
+	private final BotFavoriteMapper botFavoriteMapper;
 
 	@Autowired
-	public Viagra(BotUserItemMappingManager botUserItemMappingManager, RedisCache redisCache) {
+	public Divorce(BotUserItemMappingManager botUserItemMappingManager, BotFavoriteMapper botFavoriteMapper) {
 		this.botUserItemMappingManager = botUserItemMappingManager;
-		this.redisCache = redisCache;
+		this.botFavoriteMapper = botFavoriteMapper;
 	}
 
 	@Override
 	public String getName() {
-		return "伟哥";
+		return "休书";
 	}
 
 	@Override
 	public boolean useItem(BotSender botSender, BotUserDTO botUser, BotItem botItem) {
 		Long userId = botUser.getId();
+		BotFavorite botFavorite = botFavoriteMapper.getBotFavoriteByUserId(userId);
+		Asserts.notNull(botFavorite, "哪来的老婆");
+
 		Integer cnt = botUserItemMappingManager.addMapping(new BotUserItemMapping().setUserId(userId).setItemId(botItem.getId()).setNum(-1));
 		Asserts.checkEquals(cnt, -1, "使用失败惹");
 
-		String redisKey = String.format("CattleHandle-%s", userId);
-		redisCache.delete(redisKey);
+		botFavoriteMapper.deleteBotFavoriteByPrimary(botFavorite.getId());
 		return true;
 	}
 }
