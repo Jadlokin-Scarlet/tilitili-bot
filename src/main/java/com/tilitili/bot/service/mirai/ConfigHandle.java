7d4eaf1e54929c97d51dbd73e2daa7d012fb0d87
z-ero <w545459363@126.com>
2023-02-16 18:22:59 +0800

    favorite

--- src/main/java/com/tilitili/bot/service/mirai/ConfigHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/ConfigHandle.java
@@ -1,5 +1,6 @@
 package com.tilitili.bot.service.mirai;
 
+import com.google.common.collect.ImmutableMap;
 import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.bot.service.mirai.base.ExceptionRespMessageHandle;
 import com.tilitili.common.entity.dto.BotUserDTO;
@@ -11,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -30,14 +33,22 @@ public class ConfigHandle extends ExceptionRespMessageHandle {
     public BotMessage handleMessage(BotMessageAction messageAction) throws Exception {
         BotUserDTO botUser = messageAction.getBotUser();
         Long userId = botUser.getId();
-        String autoSellFish = messageAction.getParam("自动卖鱼");
+
+        String value = messageAction.getValue();
+        Map<String, String> valueParamMap = new HashMap<>();
+        if (value.contains(" ")) {
+            String[] valueList = value.split(" ");
+            valueParamMap.put(valueList[0], valueList[1]);
+        }
+
+        String autoSellFish = messageAction.getParamOrDefault("自动卖鱼", valueParamMap.get("自动卖鱼"));
         if (autoSellFish != null) {
             Asserts.notEquals(autoSellRepeatFishKey + userId, "yes", "你已经设置了(回收重复)");
             Asserts.isTrue(booleanTextList.contains(autoSellFish), "只能填(%s)哦", String.join(",", booleanTextList));
             redisCache.setValue(autoSellFishKey + userId, autoSellFish);
             return BotMessage.simpleTextMessage("之后掉到的鱼将会自动回收喵。");
         }
-        String autoSellRepeatFish = messageAction.getParam("回收重复");
+        String autoSellRepeatFish = messageAction.getParamOrDefault("回收重复", valueParamMap.get("回收重复"));
         if (autoSellRepeatFish != null) {
             Asserts.notEquals(autoSellFishKey + userId, "yes", "你已经设置了(自动卖鱼)");
             Asserts.isTrue(booleanTextList.contains(autoSellRepeatFish), "只能填(%s)哦", String.join(",", booleanTextList));
