6301d875849a9f2748f50435d827e8c149d9e3d3
z-ero <w545459363@126.com>
2023-02-16 18:27:20 +0800

    favorite

--- src/main/java/com/tilitili/bot/service/mirai/ConfigHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/ConfigHandle.java
@@ -5,16 +5,15 @@ import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.bot.service.mirai.base.ExceptionRespMessageHandle;
 import com.tilitili.common.entity.dto.BotUserDTO;
 import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.entity.view.bot.BotMessageChain;
 import com.tilitili.common.utils.Asserts;
 import com.tilitili.common.utils.RedisCache;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -41,19 +40,25 @@ public class ConfigHandle extends ExceptionRespMessageHandle {
             valueParamMap.put(valueList[0], valueList[1]);
         }
 
+        List<String> respList = new ArrayList<>();
+
         String autoSellFish = messageAction.getParamOrDefault("自动卖鱼", valueParamMap.get("自动卖鱼"));
         if (autoSellFish != null) {
             Asserts.notEquals(autoSellRepeatFishKey + userId, "yes", "你已经设置了(回收重复)");
             Asserts.isTrue(booleanTextList.contains(autoSellFish), "只能填(%s)哦", String.join(",", booleanTextList));
             redisCache.setValue(autoSellFishKey + userId, autoSellFish);
-            return BotMessage.simpleTextMessage("之后掉到的鱼将会自动回收喵。");
+            respList.add("之后掉到的鱼将会自动回收喵。");
         }
         String autoSellRepeatFish = messageAction.getParamOrDefault("回收重复", valueParamMap.get("回收重复"));
         if (autoSellRepeatFish != null) {
             Asserts.notEquals(autoSellFishKey + userId, "yes", "你已经设置了(自动卖鱼)");
             Asserts.isTrue(booleanTextList.contains(autoSellRepeatFish), "只能填(%s)哦", String.join(",", booleanTextList));
             redisCache.setValue(autoSellRepeatFishKey + userId, autoSellRepeatFish);
-            return BotMessage.simpleTextMessage("之后掉到的重复的鱼将会自动回收喵。");
+            respList.add("之后掉到的重复的鱼将会自动回收喵。");
+        }
+
+        if (!respList.isEmpty()) {
+            return BotMessage.simpleListMessage(respList.stream().map(BotMessageChain::ofPlain).collect(Collectors.toList()));
         }
         return null;
     }
