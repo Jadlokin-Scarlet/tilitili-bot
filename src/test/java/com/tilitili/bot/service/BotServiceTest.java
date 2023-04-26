670ac9e123d2f8164d3d5588857442d04223201c
z-ero <w545459363@126.com>
2023-04-26 14:30:13 +0800

    bili

--- src/test/java/com/tilitili/bot/service/BotServiceTest.java
+++ src/test/java/com/tilitili/bot/service/BotServiceTest.java
@@ -23,6 +23,6 @@ public class BotServiceTest {
 	@Test
 	public void syncHandleTextMessage() {
 		BotRobot bot = botRobotMapper.getBotRobotById(2L);
-		tester.testHandleMessage(bot, "{\"syncId\":\"-1\",\"data\":{\"type\":\"GroupMessage\",\"messageChain\":[{\"type\":\"Source\",\"id\":52662,\"time\":1682012918},{\"type\":\"At\",\"target\":1701008067,\"display\":\"\"},{\"type\":\"Plain\",\"text\":\" 还活着吗\"}],\"sender\":{\"id\":1578611368,\"memberName\":\"随意\",\"specialTitle\":\"\",\"permission\":\"MEMBER\",\"joinTimestamp\":1664609583,\"lastSpeakTimestamp\":1682012918,\"muteTimeRemaining\":0,\"group\":{\"id\":756782332,\"name\":\"我的评价是，润！\",\"permission\":\"OWNER\"}}}}");
+		tester.testHandleMessage(bot, "{\"syncId\":\"-1\",\"data\":{\"type\":\"FriendMessage\",\"messageChain\":[{\"type\":\"Source\",\"id\":38812,\"time\":1682490023},{\"type\":\"Plain\",\"text\":\"关注b博 425535005\"}],\"sender\":{\"id\":545459363,\"nickname\":\"Jadlokin_Scarlet\",\"remark\":\"Jadlokin_Scarlet\"}}}");
 	}
 }
