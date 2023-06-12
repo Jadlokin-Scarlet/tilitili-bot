fc65a4c78345d00423ad960d486f04c57717601e
z-ero <w545459363@126.com>
2023-06-13 06:22:10 +0800

    bot

--- src/main/java/com/tilitili/bot/socket/BaseWebSocketHandler.java
+++ src/main/java/com/tilitili/bot/socket/BaseWebSocketHandler.java
@@ -39,7 +39,7 @@ public class BaseWebSocketHandler extends WebSocketClient {
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
-        log.warn("连接关闭，60秒后尝试重连，url={} code ={}, reason={}, remote={}", this.uri.toString(), code, reason, remote);
+        log.warn("连接关闭，url={} code ={}, reason={}, remote={}", this.uri.toString(), code, reason, remote);
         if (remote) {
             executorService.schedule(() -> {
                 log.info("尝试重连");
