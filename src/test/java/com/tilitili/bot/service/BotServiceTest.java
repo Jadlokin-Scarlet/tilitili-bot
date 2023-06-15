0ccd7a3ed630a05a019d36edfc7f22a2e0b1d4a1
z-ero <w545459363@126.com>
2023-06-15 18:27:36 +0800

    mc

--- src/test/java/com/tilitili/bot/service/BotServiceTest.java
+++ src/test/java/com/tilitili/bot/service/BotServiceTest.java
@@ -24,7 +24,7 @@ public class BotServiceTest {
 	public void syncHandleTextMessage() {
 		BotRobot bot = botRobotMapper.getBotRobotById(16L);
 
-		tester.testHandleMessage(bot, "{\"player\":{\"uuid\":\"ccb182ef-08d0-33f0-85af-ac4d7b4f3e52\",\"displayName\":\"Jadlokin_Scarlet\",\"port\":3067,\"exhaustion\":0.6500002,\"exp\":0.0,\"whitelisted\":false,\"banned\":false,\"op\":false,\"balance\":1.0},\"joinMessage\":\"Jadlokin_Scarlet joined the game\",\"eventType\":\"PlayerJoin\"}");
+		tester.testHandleMessage(bot, "{\"player\":{\"uuid\":\"85bee4ae-0d36-3c26-ba60-651531963f8a\",\"displayName\":\"cirno\",\"port\":4540,\"exhaustion\":0.0,\"exp\":0.0,\"whitelisted\":false,\"banned\":false,\"op\":false,\"balance\":0.0},\"joinMessage\":\"cirno joined the game\",\"eventType\":\"PlayerJoin\"}");
 
 //		tester.testHandleMessage(botRobotMapper.getBotRobotById(12L), "{\"post_type\":\"message\",\"message_type\":\"guild\",\"time\":1685139325,\"self_id\":536657454,\"sub_type\":\"channel\",\"message\":\"[CQ:at,qq=144115218753207094] ping\",\"self_tiny_id\":\"144115218680351893\",\"sender\":{\"nickname\":\"Jadlokin_Scarlet\",\"tiny_id\":\"144115218678093982\",\"user_id\":144115218678093982},\"guild_id\":\"14133921638967841\",\"channel_id\":\"1733719\",\"message_id\":\"BAAyNroSkuohAAAAAAAadFcAAAAAAAACDQ==\",\"user_id\":\"144115218678093982\"}");
 	}
