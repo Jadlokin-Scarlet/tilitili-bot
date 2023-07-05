cecc3580b3296911eb604df141a4f8566162a896
z-ero <w545459363@126.com>
2023-07-05 19:02:24 +0800

    forward

--- src/main/java/com/tilitili/bot/service/mirai/ForwardHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/ForwardHandle.java
@@ -7,6 +7,7 @@ import com.tilitili.common.entity.BotRobot;
 import com.tilitili.common.entity.query.BotForwardConfigQuery;
 import com.tilitili.common.entity.view.bot.BotMessage;
 import com.tilitili.common.entity.view.bot.BotMessageChain;
+import com.tilitili.common.exception.AssertException;
 import com.tilitili.common.mapper.mysql.BotForwardConfigMapper;
 import com.tilitili.common.utils.StreamUtil;
 import com.tilitili.common.utils.StringUtils;
@@ -32,45 +33,6 @@ public class ForwardHandle extends BaseMessageHandleAdapt {
 		Long senderId = messageAction.getBotSender().getId();
 
 		List<BotMessageChain> sourceMessageChainList = messageAction.getBotMessage().getBotMessageChainList();
-		if (senderId == 4518L) {
-			List<BotMessageChain> botMessageChainList = sourceMessageChainList.stream()
-					.filter(StreamUtil.isEqual(BotMessageChain::getType, BotMessage.MESSAGE_TYPE_IMAGE)).collect(Collectors.toList());
-			if (!botMessageChainList.isEmpty() && blackList4370.contains(botMessageChainList.get(0).getUrl())) {
-				return null;
-			}
-			return Arrays.asList(
-					BotMessage.simpleListMessage(botMessageChainList).setSenderId(4454L),
-					BotMessage.simpleListMessage(botMessageChainList).setSenderId(4528L)
-			);
-		}
-		if (senderId == 4351L) {
-			List<BotMessageChain> botMessageChainList = sourceMessageChainList.stream()
-					.filter(StreamUtil.isEqual(BotMessageChain::getType, BotMessage.MESSAGE_TYPE_SOURCE).negate()).collect(Collectors.toList());
-			return Collections.singletonList(BotMessage.simpleListMessage(botMessageChainList).setSenderId(3759L));
-		}
-		if (senderId == 3746L) {
-			List<BotMessageChain> botMessageChainList = sourceMessageChainList.stream()
-					.filter(StreamUtil.isEqual(BotMessageChain::getType, BotMessage.MESSAGE_TYPE_SOURCE).negate()).collect(Collectors.toList());
-			return Collections.singletonList(BotMessage.simpleListMessage(botMessageChainList).setSenderId(3384L));
-		}
-
-//		for (MinecraftServerEnum server : MinecraftServerEnum.values()) {
-//			if (Objects.equals(senderId, server.getSenderId())) {
-//				String message = sourceMessageChainList.stream().map(chain -> {
-//					switch (chain.getType()) {
-//						case BotMessage.MESSAGE_TYPE_PLAIN: return chain.getText();
-//						case BotMessage.MESSAGE_TYPE_IMAGE:
-//							TranslateView translateView = baiduManager.translateImageIgnoreError(chain.getUrl());
-//							String imageText = translateView != null? translateView.getSumSrc(): null;
-//							return String.format("[图片%s]", StringUtils.isBlank(imageText)? "": ":"+ imageText);
-//						default: return "";
-//					}
-//				}).collect(Collectors.joining());
-//				String sender = messageAction.getBotUser().getName();
-//				minecraftManager.sendMessage(server, String.format("[%s]%s：%s", server.getPrefix(), sender, message));
-//				return BotMessage.emptyMessage();
-//			}
-//		}
 		BotForwardConfigQuery forwardConfigQuery = new BotForwardConfigQuery().setSourceSenderId(senderId).setStatus(0).setIsSend(true);
 		List<BotForwardConfig> forwardConfigList = botForwardConfigMapper.getBotForwardConfigByCondition(forwardConfigQuery);
 		List<BotMessage> respMessageList = new ArrayList<>();
@@ -80,34 +42,26 @@ public class ForwardHandle extends BaseMessageHandleAdapt {
 			String userName = messageAction.getBotUser().getName();
 
 			List<BotMessageChain> newMessageChainList = new ArrayList<>();
-			newMessageChainList.add(BotMessageChain.ofSpeaker(userName, sourceNameStr));
-			newMessageChainList.addAll(sourceMessageChainList.stream().map(chain -> {
-				switch (chain.getType()) {
-					case BotMessage.MESSAGE_TYPE_SOURCE: return null;
-					case BotMessage.MESSAGE_TYPE_AT: return Objects.equals(chain.getTarget().getId(), bot.getUserId()) ? null: chain;
-					default: return chain;
+			if (forwardConfig.getMessageType() == 0) {
+				newMessageChainList.add(BotMessageChain.ofSpeaker(userName, sourceNameStr));
+				newMessageChainList.addAll(sourceMessageChainList.stream().map(chain -> {
+					switch (chain.getType()) {
+						case BotMessage.MESSAGE_TYPE_SOURCE: return null;
+						case BotMessage.MESSAGE_TYPE_AT: return Objects.equals(chain.getTarget().getId(), bot.getUserId()) ? null: chain;
+						default: return chain;
+					}
+				}).filter(Objects::nonNull).collect(Collectors.toList()));
+			} else if (forwardConfig.getMessageType() == 1) {
+				newMessageChainList = sourceMessageChainList.stream()
+						.filter(StreamUtil.isEqual(BotMessageChain::getType, BotMessage.MESSAGE_TYPE_IMAGE)).collect(Collectors.toList());
+				if (!newMessageChainList.isEmpty() && blackList4370.contains(newMessageChainList.get(0).getUrl())) {
+					return null;
 				}
-			}).filter(Objects::nonNull).collect(Collectors.toList()));
+			} else {
+				throw new AssertException();
+			}
 			respMessageList.add(BotMessage.simpleListMessage(newMessageChainList).setSenderId(targetSenderId));
 		}
 		return respMessageList;
-
-//		if (BotSenderConstant.MIRAI_SENDER_ID.equals(senderId) && messageAction.getText() != null) {
-//			String text = messageAction.getText();
-//			List<String> resultList = StringUtils.extractList("(.+) 和 (.+) 开始比划牛子，", text);
-//			log.info("pkList = " + resultList);
-//			if (resultList.size() == 2 && (resultList.contains("Debris") || resultList.contains("Jadlokin_Scarlet"))) {
-//				if (future != null) {
-//					future.cancel(false);
-//				}
-//				future = scheduled.schedule(() -> {
-//					sendMessageManager.sendMessage(BotMessage.simpleListMessage(Arrays.asList(
-//							BotMessageChain.ofPlain("好啦。"),
-//							BotMessageChain.ofAt(BotUserConstant.MASTER_USER_ID)
-//					)).setBotSender(messageAction.getBotSender()));
-//				}, 5, TimeUnit.MINUTES);
-//			}
-//		}
-//		return null;
 	}
 }
