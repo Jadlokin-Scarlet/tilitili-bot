462be80d51719cdf00d2989b334ea07e924da6e1
z-ero <w545459363@126.com>
2023-10-07 18:58:14 +0800

    update

--- src/main/java/com/tilitili/bot/service/mirai/BaiduHandle.java
+++ src/main/java/com/tilitili/bot/service/mirai/BaiduHandle.java
@@ -1,10 +1,11 @@
 package com.tilitili.bot.service.mirai;
 
 import com.tilitili.bot.entity.bot.BotMessageAction;
-import com.tilitili.bot.service.ShortUrlService;
 import com.tilitili.bot.service.mirai.base.BaseMessageHandleAdapt;
+import com.tilitili.common.api.ShortUrlServiceInterface;
 import com.tilitili.common.entity.view.bot.BotMessage;
 import com.tilitili.common.utils.Asserts;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Component;
 
 import java.net.URLEncoder;
@@ -12,11 +13,8 @@ import java.nio.charset.StandardCharsets;
 
 @Component
 public class BaiduHandle extends BaseMessageHandleAdapt {
-	private final ShortUrlService shortUrlService;
-
-	public BaiduHandle(ShortUrlService shortUrlService) {
-		this.shortUrlService = shortUrlService;
-	}
+	@DubboReference
+	private ShortUrlServiceInterface shortUrlServiceInterface;
 
 	@Override
 	public BotMessage handleMessage(BotMessageAction messageAction) throws Exception {
@@ -24,10 +22,10 @@ public class BaiduHandle extends BaseMessageHandleAdapt {
 		Asserts.notBlank(value, "格式错啦(内容)");
 
 		String url = "https://www.baidu.com/s?ie=UTF-8&wd=" + URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
-		if (url.length() > 100) {
-			url = "https://www.baidu.com/s?ie=UTF-8&wd=" + value;
-		}
+//		if (url.length() > 100) {
+//			url = "https://www.baidu.com/s?ie=UTF-8&wd=" + value;
+//		}
 
-		return BotMessage.simpleLinkMessage((url));
+		return BotMessage.simpleLinkMessage(shortUrlServiceInterface.generateShortUrl(url).getUrl());
 	}
 }
