d9bd3303c233be0526ba89ac9460a6dcf7c9e8c4
z-ero <w545459363@126.com>
2024-01-14 19:51:03 +0800

    update

--- src/main/java/com/tilitili/bot/component/game/TableGame.java
+++ src/main/java/com/tilitili/bot/component/game/TableGame.java
@@ -1,9 +1,13 @@
 package com.tilitili.bot.component.game;
 
+import com.tilitili.bot.entity.bot.table.TablePlayer;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class TableGame implements Game {
+	private List<TablePlayer> playerList;
 	@Override
 	public void run() {
 
---
+++ src/main/java/com/tilitili/bot/entity/bot/table/TablePlayer.java
@@ -0,0 +1,7 @@
+package com.tilitili.bot.entity.bot.table;
+
+import com.tilitili.common.entity.dto.BaseDTO;
+
+public class TablePlayer extends BaseDTO {
+	private String name;
+}
