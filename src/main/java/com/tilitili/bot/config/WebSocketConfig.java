fc67b0745951bb3625e15042d12b31e3113fc344
z-ero <w545459363@126.com>
2024-04-07 19:33:14 +0800

    ws

--- src/main/java/com/tilitili/bot/config/WebSocketConfig.java
+++ src/main/java/com/tilitili/bot/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.tilitili.bot.config;
 import com.tilitili.bot.socket.NewWebSocketFactory;
 import com.tilitili.common.constant.BotRobotConstant;
 import com.tilitili.common.entity.BotRobot;
+import com.tilitili.common.entity.query.BotRobotQuery;
 import com.tilitili.common.manager.BotRobotCacheManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.annotation.PostConstruct;
-import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -36,9 +36,9 @@ public class WebSocketConfig {
     }
 
     private void upAllBotRobot() {
-//        List<BotRobot> robotList = botRobotCacheManager.getBotRobotByCondition(new BotRobotQuery().setStatus(0));
+        List<BotRobot> robotList = botRobotCacheManager.getBotRobotByCondition(new BotRobotQuery().setStatus(0));
 //        List<BotRobot> robotList = Arrays.asList(botRobotCacheManager.getBotRobotById(3L), botRobotCacheManager.getBotRobotById(5L));
-        List<BotRobot> robotList = Collections.singletonList(botRobotCacheManager.getBotRobotById(22L));
+//        List<BotRobot> robotList = Collections.singletonList(botRobotCacheManager.getBotRobotById(22L));
 //        List<BotRobot> robotList = Collections.emptyList();
         for (BotRobot bot : robotList) {
             if (BotRobotConstant.PUSH_TYPE_WS.equals(bot.getPushType())) {
