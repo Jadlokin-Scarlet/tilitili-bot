654f42a66315346b93da3d6397c3e26aa69c984c
z-ero <w545459363@126.com>
2024-05-07 19:21:36 +0800

    fix

--- src/main/java/com/tilitili/bot/controller/MusicController.java
+++ src/main/java/com/tilitili/bot/controller/MusicController.java
@@ -6,7 +6,6 @@ import com.tilitili.bot.service.MusicService;
 import com.tilitili.common.component.music.MusicQueueFactory;
 import com.tilitili.common.component.music.MusicRedisQueue;
 import com.tilitili.common.emnus.SendTypeEnum;
-import com.tilitili.common.entity.BotRobot;
 import com.tilitili.common.entity.BotSender;
 import com.tilitili.common.entity.BotUserSenderMapping;
 import com.tilitili.common.entity.PlayerMusicList;
@@ -21,12 +20,16 @@ import com.tilitili.common.mapper.mysql.PlayerMusicMapper;
 import com.tilitili.common.utils.Asserts;
 import com.tilitili.common.utils.RedisCache;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Slf4j
@@ -40,6 +43,7 @@ public class MusicController extends BaseController{
 	private final BotSenderCacheManager botSenderCacheManager;
 	private final BotRobotCacheManager botRobotCacheManager;
 	private final RedisCache redisCache;
+	private final List<SseEmitter> emitterList = new ArrayList<>();
 
 	public MusicController(MusicService musicService, PlayerMusicMapper playerMusicMapper, PlayerMusicListMapper playerMusicListMapper, BotUserSenderMappingMapper botUserSenderMappingMapper, BotSenderCacheManager botSenderCacheManager, BotRobotCacheManager botRobotCacheManager, RedisCache redisCache) {
 		this.musicService = musicService;
@@ -65,11 +69,40 @@ public class MusicController extends BaseController{
 		BotUserSenderMapping mapping = mappingList.stream().findFirst().orElse(null);
 		Asserts.notNull(mapping, "你好像还没加入语音");
 		BotSender botSender = botSenderCacheManager.getValidBotSenderById(mapping.getSenderId());
-		BotRobot bot = botRobotCacheManager.getValidBotRobotById(botSender.getBot());
-		MusicRedisQueue musicRedisQueue = MusicQueueFactory.getQueueInstance(bot, redisCache);
+		MusicRedisQueue musicRedisQueue = MusicQueueFactory.getQueueInstance(botSender.getBot(), redisCache);
 		PlayerMusicDTO theMusic = musicRedisQueue.getTheMusic();
 		ListPlayerMusicResponse response = new ListPlayerMusicResponse();
 		response.setTheMusic(theMusic);
 		return BaseModel.success(response);
 	}
+
+	@RequestMapping("/player/event")
+	public SseEmitter registerSubscribe() {
+		SseEmitter emitter = new SseEmitter();
+		emitterList.add(emitter);
+
+		emitter.onCompletion(()->emitterList.remove(emitter));
+		emitter.onTimeout(()->emitterList.remove(emitter));
+
+		return emitter;
+	}
+
+	@JmsListener(destination = "KtvUpdateMessage")
+	public void ktvUpdateMessage(Long botId) {
+		MusicRedisQueue musicRedisQueue = MusicQueueFactory.getQueueInstance(botId, redisCache);
+		PlayerMusicDTO theMusic = musicRedisQueue.getTheMusic();
+		ListPlayerMusicResponse response = new ListPlayerMusicResponse();
+		response.setTheMusic(theMusic);
+
+		for (Iterator<SseEmitter> iterator = this.emitterList.iterator(); iterator.hasNext(); ) {
+			SseEmitter emitter = iterator.next();
+			try {
+				emitter.send(response);
+			} catch (Exception e) {
+				log.warn("下发事件异常，移除emitter", e);
+				iterator.remove();
+			}
+		}
+	}
+
 }
--- src/main/java/com/tilitili/bot/receive/MinecraftReceive.java
+++ src/main/java/com/tilitili/bot/receive/MinecraftReceive.java
@@ -1,96 +1,79 @@
 package com.tilitili.bot.receive;
 
-import com.google.gson.Gson;
-import com.tilitili.bot.service.BotService;
-import com.tilitili.common.emnus.TaskReason;
-import com.tilitili.common.emnus.TaskStatus;
-import com.tilitili.common.entity.BotRobot;
-import com.tilitili.common.entity.BotSender;
-import com.tilitili.common.entity.view.message.TaskMessage;
-import com.tilitili.common.exception.AssertException;
-import com.tilitili.common.manager.BotRobotCacheManager;
-import com.tilitili.common.manager.BotSenderCacheManager;
-import com.tilitili.common.mapper.rank.TaskMapper;
-import com.tilitili.common.utils.Asserts;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.env.Environment;
-import org.springframework.jms.UncategorizedJmsException;
-import org.springframework.jms.core.JmsTemplate;
-
-import java.util.List;
 
 @Slf4j
 public class MinecraftReceive {
-	private final String ip;
-	private final Gson gson;
-	private final JmsTemplate jmsTemplate;
-	private final TaskMapper taskMapper;
-	private final BotService botService;
-	private final BotSenderCacheManager botSenderCacheManager;
-	private final BotRobotCacheManager botRobotCacheManager;
-
-
-	public MinecraftReceive(JmsTemplate jmsTemplate, TaskMapper taskMapper, Environment environment, BotService botService, BotSenderCacheManager botSenderCacheManager, BotRobotCacheManager botRobotCacheManager) {
-		this.botService = botService;
-		this.botSenderCacheManager = botSenderCacheManager;
-		this.botRobotCacheManager = botRobotCacheManager;
-		gson = new Gson();
-		this.ip = environment.getProperty("ip");
-		this.jmsTemplate = jmsTemplate;
-		this.taskMapper = taskMapper;
-	}
-
+//	private final String ip;
+//	private final Gson gson;
+//	private final JmsTemplate jmsTemplate;
+//	private final TaskMapper taskMapper;
+//	private final BotService botService;
+//	private final BotSenderCacheManager botSenderCacheManager;
+//	private final BotRobotCacheManager botRobotCacheManager;
+//
+//
+//	public MinecraftReceive(JmsTemplate jmsTemplate, TaskMapper taskMapper, Environment environment, BotService botService, BotSenderCacheManager botSenderCacheManager, BotRobotCacheManager botRobotCacheManager) {
+//		this.botService = botService;
+//		this.botSenderCacheManager = botSenderCacheManager;
+//		this.botRobotCacheManager = botRobotCacheManager;
+//		gson = new Gson();
+//		this.ip = environment.getProperty("ip");
+//		this.jmsTemplate = jmsTemplate;
+//		this.taskMapper = taskMapper;
+//	}
+//
 //	@Scheduled(fixedDelay = 1)
-	public void  receiveMinecraftMessage() {
-		TaskMessage taskMessage = null;
-		try {
-			taskMessage = this.getTaskMessage();
-			if (taskMessage == null) {
-				return;
-			}
-			Long taskId = taskMessage.getId();
-			List<String> valueList = taskMessage.getValueList();
-			Asserts.isTrue(valueList.size() == 2, "参数异常");
-			String requestStr = valueList.get(0);
-			String senderIdStr = valueList.get(1);
-			Asserts.isNumber(senderIdStr, "参数异常");
-			log.info("Message Received {}",requestStr);
-			long senderId = Long.parseLong(senderIdStr);
-			BotSender botSender = botSenderCacheManager.getValidBotSenderById(senderId);
-			Asserts.notNull(botSender, "权限不足");
-			BotRobot bot = botRobotCacheManager.getValidBotRobotById(botSender.getBot());
-			Asserts.notNull(bot, "权限不足");
-			botService.syncHandleMessage(bot, requestStr);
-			taskMapper.updateStatusById(taskId, TaskStatus.SPIDER.getValue(), TaskStatus.SUCCESS.getValue());
-		} catch (AssertException e) {
-			if (taskMessage != null && taskMessage.getId() != null) {
-				taskMapper.updateStatusById(taskMessage.getId(), TaskStatus.SPIDER.getValue(), TaskStatus.FAIL.getValue());
-			}
-			log.warn("消费mc消息断言异常, message=" + gson.toJson(taskMessage), e);
-		} catch (Exception e) {
-			if (taskMessage != null && taskMessage.getId() != null) {
-				taskMapper.updateStatusById(taskMessage.getId(), TaskStatus.SPIDER.getValue(), TaskStatus.FAIL.getValue());
-			}
-			log.error("消费mc消息异常, message=" + gson.toJson(taskMessage), e);
-		}
-	}
-
-	private TaskMessage getTaskMessage() {
-		TaskMessage taskMessage;
-		try {
-			taskMessage = (TaskMessage) jmsTemplate.receiveAndConvert(TaskReason.MINECRAFT_MESSAGE.destination);
-		} catch (UncategorizedJmsException e) {
-			log.warn("消息等待中断", e);
-			return null;
-		} catch (Exception e) {
-			log.error("消息接收异常", e);
-			return null;
-		}
-		if (taskMessage == null) {
-			return null;
-		}
-		taskMapper.updateStatusAndIpById(taskMessage.getId(), TaskStatus.WAIT.getValue(), TaskStatus.SPIDER.getValue(), ip);
-		return taskMessage;
-	}
+//	public void  receiveMinecraftMessage() {
+//		TaskMessage taskMessage = null;
+//		try {
+//			taskMessage = this.getTaskMessage();
+//			if (taskMessage == null) {
+//				return;
+//			}
+//			Long taskId = taskMessage.getId();
+//			List<String> valueList = taskMessage.getValueList();
+//			Asserts.isTrue(valueList.size() == 2, "参数异常");
+//			String requestStr = valueList.get(0);
+//			String senderIdStr = valueList.get(1);
+//			Asserts.isNumber(senderIdStr, "参数异常");
+//			log.info("Message Received {}",requestStr);
+//			long senderId = Long.parseLong(senderIdStr);
+//			BotSender botSender = botSenderCacheManager.getValidBotSenderById(senderId);
+//			Asserts.notNull(botSender, "权限不足");
+//			BotRobot bot = botRobotCacheManager.getValidBotRobotById(botSender.getBot());
+//			Asserts.notNull(bot, "权限不足");
+//			botService.syncHandleMessage(bot, requestStr);
+//			taskMapper.updateStatusById(taskId, TaskStatus.SPIDER.getValue(), TaskStatus.SUCCESS.getValue());
+//		} catch (AssertException e) {
+//			if (taskMessage != null && taskMessage.getId() != null) {
+//				taskMapper.updateStatusById(taskMessage.getId(), TaskStatus.SPIDER.getValue(), TaskStatus.FAIL.getValue());
+//			}
+//			log.warn("消费mc消息断言异常, message=" + gson.toJson(taskMessage), e);
+//		} catch (Exception e) {
+//			if (taskMessage != null && taskMessage.getId() != null) {
+//				taskMapper.updateStatusById(taskMessage.getId(), TaskStatus.SPIDER.getValue(), TaskStatus.FAIL.getValue());
+//			}
+//			log.error("消费mc消息异常, message=" + gson.toJson(taskMessage), e);
+//		}
+//	}
+//
+//	private TaskMessage getTaskMessage() {
+//		TaskMessage taskMessage;
+//		try {
+//			taskMessage = (TaskMessage) jmsTemplate.receiveAndConvert(TaskReason.MINECRAFT_MESSAGE.destination);
+//		} catch (UncategorizedJmsException e) {
+//			log.warn("消息等待中断", e);
+//			return null;
+//		} catch (Exception e) {
+//			log.error("消息接收异常", e);
+//			return null;
+//		}
+//		if (taskMessage == null) {
+//			return null;
+//		}
+//		taskMapper.updateStatusAndIpById(taskMessage.getId(), TaskStatus.WAIT.getValue(), TaskStatus.SPIDER.getValue(), ip);
+//		return taskMessage;
+//	}
 
 }
