094dcba79255b182c4fb1b6c7ab543127da857c8
z-ero <w545459363@126.com>
2024-05-14 13:00:14 +0800

    fix

--- src/main/java/com/tilitili/bot/config/CorsConfig.java
+++ src/main/java/com/tilitili/bot/config/CorsConfig.java
@@ -15,19 +15,19 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Bean
 	public CorsFilter corsFilter() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		this.configPul(source);
+		this.configPub(source);
 		this.configDefault(source);
 		return new CorsFilter(source);
 	}
 
-	private void configPul(UrlBasedCorsConfigurationSource source) {
+	private void configPub(UrlBasedCorsConfigurationSource source) {
 		final CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
 		config.setAllowedOriginPatterns(Collections.singletonList("*"));
 		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept","Authorization", "Cache-Control"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"));
 
-		source.registerCorsConfiguration("/api/**", config);
+		source.registerCorsConfiguration("/api/pub/**", config);
 	}
 
 	private void configDefault(UrlBasedCorsConfigurationSource source) {
