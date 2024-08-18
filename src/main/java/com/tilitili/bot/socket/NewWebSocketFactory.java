4a197e3be74e762dff6bdbb9edcdb2a409678732
z-ero <w545459363@126.com>
2024-08-18 19:06:40 +0800

    update

--- src/main/java/com/tilitili/bot/socket/NewWebSocketFactory.java
+++ src/main/java/com/tilitili/bot/socket/NewWebSocketFactory.java
@@ -56,7 +56,7 @@ public class NewWebSocketFactory {
 				if (!webSocketMap.containsKey(botId)) {
 					Asserts.isFalse(redisCache.exists("ws_lock_"+botId), "过于频繁");
 					Long wsCnt = redisCache.increment("ws_cnt_" + botId);
-					redisCache.expire("ws_cnt_"+botId, 60);
+					redisCache.expire("ws_cnt_"+botId, 50);
 					if (wsCnt > 10) {
 						redisCache.setValue("ws_lock_"+botId, "yes", 60 * 10);
 					}
