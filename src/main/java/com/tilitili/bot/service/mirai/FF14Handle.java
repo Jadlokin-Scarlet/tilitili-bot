43ef5a52b76da18a2e2ae7ba08114085ae6751ec
z-ero <w545459363@126.com>
2024-10-08 22:46:13 +0800

    update

--- src/main/java/com/tilitili/bot/service/mirai/FF14Handle.java
+++ src/main/java/com/tilitili/bot/service/mirai/FF14Handle.java
@@ -2,6 +2,7 @@ package com.tilitili.bot.service.mirai;
 
 import com.alibaba.fastjson2.JSONException;
 import com.alibaba.fastjson2.JSONPath;
+import com.alibaba.fastjson2.TypeReference;
 import com.google.common.collect.ImmutableMap;
 import com.tilitili.bot.entity.bot.BotMessageAction;
 import com.tilitili.bot.service.mirai.base.ExceptionRespMessageHandle;
@@ -18,13 +19,18 @@ import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class FF14Handle extends ExceptionRespMessageHandle {
     private final Map<String, String> headers = ImmutableMap.of("Referer", "https://universalis.app/", "user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36", "cookie", "SL_G_WPT_TO=zh-CN; __Secure-next-auth.callback-url=https%3A%2F%2Funiversalis.app; __Host-next-auth.csrf-token=5013d4642b540a466e4b0c69eb2f13795ea4909accf8431b3c03fc5d57207fce%7Cb89bffe443901456e5487a6df11f45173ef59431b7f5183e839d64a92ea3c531; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; mogboard_server=%E7%B4%AB%E6%B0%B4%E6%A0%88%E6%A1%A5; mogboard_language=chs; mogboard_timezone=Asia%2FShanghai; mogboard_leftnav=on; mogboard_homeworld=yes; mogboard_last_selected_server=%E7%B4%AB%E6%B0%B4%E6%A0%88%E6%A1%A5");
+    private final JSONPath searchPath = JSONPath.of("$.Results[0].ID",  Integer.class);
+    private final List<String> wordList = Arrays.asList("1169", "1045", "1178", "1177", "1179", "1043", "1106");
 
     @Override
     public BotMessage handleMessage(BotMessageAction messageAction) throws Exception {
@@ -33,12 +39,17 @@ public class FF14Handle extends ExceptionRespMessageHandle {
         if (id == null) {
             return null;
         }
-        UniversalisItemPrice itemPrice = this.getPriceList(id);
-        Asserts.notNull(itemPrice, "查询失败");
-        return BotMessage.simpleTextMessage(String.format("%s的最低价位%d，更新于%s（包括 5%% 消费税）", value, itemPrice.getMinPrice(), formatDate(itemPrice.getLastUploadTime())));
+        Map<String, UniversalisItemPrice> itemPriceMap = this.getPriceList(id);
+        Asserts.notNull(itemPriceMap, "查询失败");
+        String resp = wordList.stream().map(itemPriceMap::get).map(itemPrice -> (
+                String.format("%s的最低价为%d%s，更新于%s", itemPrice.getWorldName(), itemPrice.getMinPrice()
+                        , itemPrice.getMinPriceHQ() == 0? "": String.format("(HQ:%d)", itemPrice.getMinPriceHQ())
+                        , formatDate(itemPrice.getLastUploadTime()))
+        )).collect(Collectors.joining("\n")) + "\n（包括 5%% 消费税）";
+        return BotMessage.simpleTextMessage(resp);
+//        return BotMessage.simpleTextMessage(String.format("%s的最低价位%d，更新于%s（包括 5%% 消费税）", value, itemPrice.getMinPrice(), formatDate(itemPrice.getLastUploadTime())));
     }
 
-    JSONPath searchPath = JSONPath.of("$.Results[0].ID",  Integer.class);
     private Integer search(String name) throws UnsupportedEncodingException {
         String url = "https://cafemaker.wakingsands.com/search?string="+ URLEncoder.encode(name, "utf-8") +"&indexes=item&language=chs&filters=ItemSearchCategory.ID%3E=1&columns=ID,Icon,Name,LevelItem,Rarity,ItemSearchCategory.Name,ItemSearchCategory.ID,ItemKind.Name&limit=100&sort_field=LevelItem&sort_order=desc";
         HttpResponseDTO resp = HttpClientUtil.httpGet(HttpRequestDTO.ofUrlAndHeader(url, headers).setHttpClient(HttpClientUtil.longHttpClient));
@@ -53,16 +64,21 @@ public class FF14Handle extends ExceptionRespMessageHandle {
         }
     }
 
-    JSONPath priceListPath = JSONPath.of("$.props.pageProps.markets.1043",  UniversalisItemPrice.class);
-    private UniversalisItemPrice getPriceList(Integer id) {
-        String html = HttpClientUtil.httpGet("https://universalis.app/market/" + id, headers);
+    JSONPath priceListPath = JSONPath.of("$.props.pageProps.markets",  new TypeReference<Map<String, UniversalisItemPrice>>(){}.getType());
+    private Map<String, UniversalisItemPrice> getPriceList(Integer id) {
+        String html = HttpClientUtil.httpGet(HttpRequestDTO.ofUrlAndHeader("https://universalis.app/market/" + id, headers).setHttpClient(HttpClientUtil.longHttpClient)).getContent();
         Asserts.notBlank(html, "网络异常");
         Document document = Jsoup.parse(html);
-
         String json = document.select("script#__NEXT_DATA__").first().data();
+//		String json = null;
+//		try {
+//			json = IOUtils.toString(FF14Handle.class.getResourceAsStream("/data.json"), StandardCharsets.UTF_8);
+//		} catch (IOException e) {
+//			throw new RuntimeException(e);
+//		}
         Asserts.notBlank(json, "网络异常");
         try {
-            return (UniversalisItemPrice) priceListPath.extract(json);
+            return (Map<String, UniversalisItemPrice>) priceListPath.extract(json);
         } catch (JSONException e) {
             log.warn("error handle json="+json, e);
             return null;
