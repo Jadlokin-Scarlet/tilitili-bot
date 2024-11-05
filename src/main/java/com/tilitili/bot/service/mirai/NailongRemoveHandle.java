7d57c59c343373871e4748f87318fa73ab3e2089
z-ero <w545459363@126.com>
2024-11-05 22:52:24 +0800

    update

---
+++ src/main/java/com/tilitili/bot/service/mirai/NailongRemoveHandle.java
@@ -0,0 +1,29 @@
+package com.tilitili.bot.service.mirai;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.tilitili.bot.entity.bot.BotMessageAction;
+import com.tilitili.bot.service.mirai.base.ExceptionRespMessageHandle;
+import com.tilitili.common.entity.view.bot.BotMessage;
+import com.tilitili.common.utils.Asserts;
+import com.tilitili.common.utils.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class NailongRemoveHandle extends ExceptionRespMessageHandle {
+	@Override
+	public BotMessage handleMessage(BotMessageAction messageAction) throws Exception {
+		List<String> imageList = messageAction.getImageList();
+		for (String imageUrl : imageList) {
+			String result = HttpClientUtil.httpPost("http://172.27.0.7:8081/check_image?image_url=" + imageUrl);
+			log.info("check image url:{} result:{}", imageUrl, result);
+			Asserts.notBlank(result, "网络异常");
+			Boolean checkOk = JSONObject.parseObject(result).getBoolean("check_ok");
+			log.info("check ok:{}", checkOk);
+		}
+		return null;
+	}
+}
